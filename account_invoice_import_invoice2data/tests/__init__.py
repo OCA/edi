7dee4f0ae49bd204df15e60871e4745108682df9
Alexis de Lattre <alexis.delattre@akretion.com>
2017-01-07 22:41:00 +0100

    Disable tests for invoice2data because it's too difficult to install a recent version of pdftotext on travis's ubuntu 12.04 images

--- account_invoice_import_invoice2data/tests/__init__.py
+++ account_invoice_import_invoice2data/tests/__init__.py
@@ -2,5 +2,9 @@
 
 # The test below works well, but the full installation of invoice2data
 # with the special version of the pdftotext binary is difficult
-# and I don't know how to put it in the configuration of Travis
-from . import test_invoice_import
+# Invoice2data new requires a recent version of poppler-utils
+# from https://poppler.freedesktop.org/releases.html
+# But Travis build environnement are based on Ubuntu 12.04 and I didn't
+# find any PPA for poppler-utils for 12.04 and it doesn't seem easy to have a
+# static build
+#from . import test_invoice_import
