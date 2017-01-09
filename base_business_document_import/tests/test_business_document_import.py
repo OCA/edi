3309d5755361d71b1896e2aed6074c4fa0ba8c92
Holger Brunn <hbrunn@therp.nl>
2017-01-09 08:45:16 +0100

    product.product#ean13 was renamed to barcode

--- base_business_document_import/tests/test_business_document_import.py
+++ base_business_document_import/tests/test_business_document_import.py
@@ -115,7 +115,7 @@ class TestBaseBusinessDocumentImport(TransactionCase):
         ppo = self.env['product.product']
         product1 = ppo.create({
             'name': u'Test Product',
-            'ean13': '9782203121102',
+            'barcode': '9782203121102',
             'seller_ids': [
                 (0, 0, {
                     'name': self.env.ref('base.res_partner_2').id,
@@ -126,7 +126,7 @@ class TestBaseBusinessDocumentImport(TransactionCase):
         product_dict = {'code': u'A2324 '}
         res = bdio._match_product(product_dict, [])
         self.assertEquals(res, self.env.ref('product.product_product_4b'))
-        product_dict = {'ean13': u'9782203121102'}
+        product_dict = {'barcode': u'9782203121102'}
         res = bdio._match_product(product_dict, [])
         self.assertEquals(res, product1)
         product_dict = {'code': 'TEST1242'}
