9e4e264851a9f276797e891dd1f5e4aca24d2fbd
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-15 18:46:54 +0100

    Port base_business_document_import to v10
    Add match for accounts and journals

--- base_business_document_import/tests/test_business_document_import.py
+++ base_business_document_import/tests/test_business_document_import.py
@@ -2,7 +2,7 @@
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestBaseBusinessDocumentImport(TransactionCase):
