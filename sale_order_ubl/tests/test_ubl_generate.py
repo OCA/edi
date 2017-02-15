21010b44325df53cec7f58152a287d1b9f08afba
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-15 15:11:22 +0100

    Port to v10
    Prepare v10 branch
    
    Rename __openerp__.py to __manifest__.py and set installable to False
    
    Also port all the modules that generate the XML documents: account_invoice_ubl, account_invoice_zugferd, purchase_order_ubl and sale_order_ubl
    
    Rename account_invoice_zugferd to account_invoice_factur-x
    
    Rename account_invoice_import_zugferd to account_invoice_import_factur-x
    Add module to support py3o reporting engine:
    
    Continue port of modules for v10.0, in particular sale_order_import_* module
    
    Fix spelling mistake and other remarks on README by Tarteo
    
    Disable get_pdf() in all tests because it doesn't work in Travis
    
    [10.0] restore get_pdf() in tests (#31)
    
    * sale_order_ubl + purchase_order_ubl: restore get_pdf() in tests using HttpCase
    
    * Restore get_pdf() in tests of account_invoice_factur-x and account_invoice_ubl modules
    
    * Update oca_dependencies.txt

--- sale_order_ubl/tests/test_ubl_generate.py
+++ sale_order_ubl/tests/test_ubl_generate.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import HttpCase
 
 
-class TestUblOrderImport(TransactionCase):
+class TestUblOrderImport(HttpCase):
 
     def test_ubl_generate(self):
-        ro = self.registry['report']
+        ro = self.env['report']
         soo = self.env['sale.order']
         buo = self.env['base.ubl']
         quotation_states = soo.get_quotation_states()
         order_states = soo.get_order_states()
-        for i in range(7):
+        for i in range(8):
             i += 1
             order = self.env.ref('sale.sale_order_%d' % i)
             for version in ['2.0', '2.1']:
-                # I didn't manage to make it work with new api :-(
-                pdf_file = ro.get_pdf(
-                    self.cr, self.uid, order.ids, 'sale.report_saleorder',
-                    context={'ubl_version': version})
+                pdf_file = ro.with_context(ubl_version=version).get_pdf(
+                    order.ids, 'sale.report_saleorder')
                 res = buo.get_xml_files_from_pdf(pdf_file)
                 if order.state in quotation_states:
                     filename = order.get_ubl_filename(
