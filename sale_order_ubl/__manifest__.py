dfc18adf415516c539d83391ec156d7391cd39fb
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-15 15:11:22 +0100

    Port to v10
    Prepare v10 branch
    
    Rename __openerp__.py to __manifest__.py and set installable to False
    
    Also port all the modules that generate the XML documents: account_invoice_ubl, account_invoice_zugferd, purchase_order_ubl and sale_order_ubl
    
    Rename account_invoice_zugferd to account_invoice_factur-x
    
    Rename account_invoice_import_zugferd to account_invoice_import_factur-x
    Add module to support py3o reporting engine:
    
    Continue port of modules for v10.0, in particular sale_order_import_* module
    
    Fix spelling mistake and other remarks on README by Tarteo
    
    Disable get_pdf() in all tests because it doesn't work in Travis
    
    [10.0] restore get_pdf() in tests (#31)
    
    * sale_order_ubl + purchase_order_ubl: restore get_pdf() in tests using HttpCase
    
    * Restore get_pdf() in tests of account_invoice_factur-x and account_invoice_ubl modules
    
    * Update oca_dependencies.txt

--- sale_order_ubl/__manifest__.py
+++ sale_order_ubl/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Order UBL',
-    'version': '8.0.1.0.0',
-    'category': 'Sales Management',
+    'version': '10.0.1.0.0',
+    'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Embed UBL XML file inside the PDF quotation',
     'author': 'Akretion,Odoo Community Association (OCA)',
