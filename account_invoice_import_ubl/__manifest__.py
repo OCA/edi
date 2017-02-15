c67868f02e28e906b066fc69e6d4d871a1bc83fa
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-15 15:11:22 +0100

    Port account_invoice_import_ubl to v10.0
    Prepare v10 branch
    
    Rename __openerp__.py to __manifest__.py and set installable to False
    
    Port account_invoice_import_factur-x and account_invoice_import_ubl to v10.0
    
    base_business_document_import: Add support for the creation of res.bank
    
    Port purchase_order_import* to v10.0
    
    Add ubl invoice generation option in accounting config page

--- account_invoice_import_ubl/__manifest__.py
+++ account_invoice_import_ubl/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Import UBL',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Import UBL XML supplier invoices/refunds',
