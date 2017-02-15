ec4d8f6ff6d62344ae4bf91f4165ab147b2fb3c9
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-15 15:11:22 +0100

    Port base_business_document_import* to v10
    Also port all the modules that generate the XML documents: account_invoice_ubl, account_invoice_zugferd, purchase_order_ubl and sale_order_ubl
    
    Add active buttons on invoice import config
    
    Note: it requires this fix on odoo 10: https://github.com/odoo/odoo/pull/15649
    
    Port account_invoice_import_factur-x and account_invoice_import_ubl to v10.0
    
    base_business_document_import: Add support for the creation of res.bank
    
    Port purchase_order_import* to v10.0
    
    Add ubl invoice generation option in accounting config page
    
    Update to OCA coding standards

--- account_invoice_import/models/partner.py
+++ account_invoice_import/models/partner.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ResPartner(models.Model):
