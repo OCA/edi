0dbf2a8785f92bebebbc5ffabe82547180216239
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-23 00:43:59 +0100

    Rename account_invoice_zugferd to account_invoice_factur-x
    Rename account_invoice_import_zugferd to account_invoice_import_factur-x
    Add module to support py3o reporting engine:

---
+++ account_invoice_factur-x/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Invoice Factur-X',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Generate Factur-X/ZUGFeRD customer invoices',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['account_payment_partner', 'base_zugferd', 'base_vat'],
+    'external_dependencies': {'python': ['PyPDF2', 'lxml']},
+    'installable': True,
+}
