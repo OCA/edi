e364dde6a2c7a5fc1d63b7b05cc986fc1d1bcd64
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-23 00:43:59 +0100

    Rename account_invoice_zugferd to account_invoice_factur-x
    Rename account_invoice_import_zugferd to account_invoice_import_factur-x
    Add module to support py3o reporting engine:

---
+++ account_invoice_ubl_py3o/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Py3o UBL Invoice',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'summary': 'Generate UBL invoices with Py3o reporting engine',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['account_invoice_ubl', 'report_py3o'],
+    'installable': True,
+}
---
+++ account_invoice_ubl_py3o/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import py3o_report
