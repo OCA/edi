4a69a4d68617e844e46810c3d2d38e2be7a3890b
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-27 23:23:59 +0100

    Continue port of modules for v10.0, in particular sale_order_import_* module
    Fix spelling mistake and other remarks on README by Tarteo

--- sale_order_import/__manifest__.py
+++ sale_order_import/__manifest__.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Order Import',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Import RFQ or sale orders from files',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
-    'depends': ['sale_commercial_partner', 'base_business_document_import'],
+    'depends': [
+        'sale_commercial_partner',
+        'base_business_document_import',
+        'onchange_helper',
+        ],
     'data': [
         'wizard/sale_order_import_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
