38b423404f58d8b86283050682275b6632ff8bd7
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-28 09:24:46 +0100

    Port account_invoice_import_factur-x and account_invoice_import_ubl to v10.0
    base_business_document_import: Add support for the creation of res.bank

--- sale_order_import/models/sale.py
+++ sale_order_import/models/sale.py
@@ -2,7 +2,7 @@
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api, _
+from odoo import models, _
 
 
 class SaleOrder(models.Model):
