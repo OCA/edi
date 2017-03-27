b83d6f0588ce48b921af81454dc3299b5b398f21
Alexis de Lattre <alexis.delattre@akretion.com>
2017-03-27 20:20:03 +0200

    Allow importing invoice without invoice_import_config on partner (usefull for third-party modules that use account_invoice_import)
    Code cleanup and optimisations

--- account_invoice_import/models/partner.py
+++ account_invoice_import/models/partner.py
@@ -2,7 +2,8 @@
 # © 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, _
+from odoo.exceptions import UserError
 
 
 class ResPartner(models.Model):
@@ -12,3 +13,27 @@ class ResPartner(models.Model):
         'account.invoice.import.config',
         string='Invoice Import Configuration',
         company_dependent=True)
+
+    def invoice_import2import_config(self):
+        self.ensure_one()
+        if not self.invoice_import_id:
+            raise UserError(_(
+                "Missing Invoice Import Configuration on partner '%s'.")
+                % self.name_get()[0][1])
+        config = self.invoice_import_id
+        vals = {
+            'invoice_line_method': config.invoice_line_method,
+            'account_analytic': config.account_analytic_id or False,
+            }
+        if config.invoice_line_method == '1line_no_product':
+            vals['account'] = config.account_id
+            vals['taxes'] = config.tax_ids
+            vals['label'] = config.label or False
+        elif config.invoice_line_method == '1line_static_product':
+            vals['product'] = config.product_id
+            vals['label'] = config.label or False
+        elif config.invoice_line_method == 'nline_no_product':
+            vals['account'] = config.account_id
+        elif config.invoice_line_method == 'nline_static_product':
+            vals['product'] = config.product_id
+        return vals
