889602907de170c4738d55bf203291ad6f4c981d
kiplangatdan <kiplangatdan@gmail.com>
2017-04-27 21:48:36 +0300

    Ported to 9.0

--- sale_order_import/__openerp__.py
+++ sale_order_import/__openerp__.py
@@ -3,16 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Sale Order Import',
-    'version': '8.0.1.0.0',
+    'name': 'Sale Order Import - Dan',
+    'version': '9.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Import RFQ or sale orders from files',
-    'author': 'Akretion,Odoo Community Association (OCA)',
+    'author': 'Akretion,Odoo Community Association (OCA), Sunflower IT',
     'website': 'http://www.akretion.com',
-    'depends': ['sale_commercial_partner', 'base_business_document_import'],
+    'depends': ['sale'],
+    # 'depends': ['sale_commercial_partner', 'base_business_document_import'],
     'data': [
         'wizard/sale_order_import_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
