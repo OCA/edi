c7daaaddf35a3963f614ff8153597f66eb8ab91c
DEEPAK KUMAR <deepak.kumar.serpentcs.com>
2017-05-09 14:50:00 +0530

    [IMP] Migrated sale_commercial_partner, sale_order_import Modules.

--- sale_commercial_partner/__openerp__.py
+++ sale_commercial_partner/__openerp__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2017-Today Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Commercial Partner',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': "Add stored related field 'Commercial Customer' on sale orders",
@@ -15,5 +17,5 @@
         'views/sale.xml',
         'report/sale_report_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
--- sale_order_import/__openerp__.py
+++ sale_order_import/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2017-Today Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     'name': 'Sale Order Import',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Import RFQ or sale orders from files',
@@ -14,5 +15,5 @@
     'data': [
         'wizard/sale_order_import_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
