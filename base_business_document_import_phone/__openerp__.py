c8f6ee42a0e7c562b169d06e1e36fc086ecddfdf
DEEPAK KUMAR <deepak.kumar.serpentcs.com>
2017-05-17 13:20:04 +0530

    [IMP]Migrated Module In Version 9

--- base_business_document_import_phone/__openerp__.py
+++ base_business_document_import_phone/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2017-Today Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     'name': 'Base Business Document Import Phone',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Use phone numbers to match partners upon import of '
@@ -15,6 +16,6 @@
         'base_phone',
         'base_business_document_import',
         ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
 }
