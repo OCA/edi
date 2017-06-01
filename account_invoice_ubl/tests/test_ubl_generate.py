ef5155b7b6b53e0eacf919d9e791d24e86facbc1
Alexis de Lattre <alexis.delattre@akretion.com>
2017-06-01 13:41:59 +0200

    Update test suite

--- account_invoice_ubl/tests/test_ubl_generate.py
+++ account_invoice_ubl/tests/test_ubl_generate.py
@@ -2,26 +2,20 @@
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.addons.account_payment_unece.tests.test_account_invoice import \
+    TestAccountInvoice
 
 
-class TestUblInvoice(TransactionCase):
+class TestUblInvoice(TestAccountInvoice):
 
     def test_ubl_generate(self):
-        ro = self.registry['report']
+        ro = self.env['report']
         buo = self.env['base.ubl']
-        for i in range(5):
-            i += 1
-            invoice = self.env.ref('account.invoice_%d' % i)
-            # validate invoice
-            invoice.action_invoice_open()
-            if invoice.type not in ('out_invoice', 'out_refund'):
-                continue
-            for version in ['2.0', '2.1']:
-                # I didn't manage to make it work with new api :-(
-                pdf_file = ro.get_pdf(
-                    self.cr, self.uid, invoice.ids,
-                    'account.report_invoice', context={'ubl_version': version})
-                res = buo.get_xml_files_from_pdf(pdf_file)
-                invoice_filename = invoice.get_ubl_filename(version=version)
-                self.assertTrue(invoice_filename in res)
+        invoice = self.test_only_create_invoice()
+        for version in ['2.0', '2.1']:
+            # I didn't manage to make it work with new api :-(
+            pdf_file = ro.with_context(ubl_version=version).get_pdf(
+                [invoice.id], 'account.report_invoice')
+            res = buo.get_xml_files_from_pdf(pdf_file)
+            invoice_filename = invoice.get_ubl_filename(version=version)
+            self.assertTrue(invoice_filename in res)
