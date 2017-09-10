ea55f01681a11e555126914690fb27f4046b9c1e
Alexis de Lattre <alexis.delattre@akretion.com>
2017-09-10 21:00:04 +0200

    Adapt to final version of factur-x lib

--- account_invoice_factur-x/__manifest__.py
+++ account_invoice_factur-x/__manifest__.py
@@ -11,7 +11,7 @@
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'depends': ['account_payment_partner', 'base_zugferd', 'base_vat'],
-    'external_dependencies': {'python': ['FacturX']},
+    'external_dependencies': {'python': ['facturx']},
     'data': ['views/res_partner.xml'],
     'installable': True,
 }
