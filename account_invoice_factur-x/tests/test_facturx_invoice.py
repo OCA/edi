2859a77cb8c611b9b9b60a1823e56cac98cf5b8a
Alexis de Lattre <alexis.delattre@akretion.com>
2017-10-14 16:44:51 +0200

    Add logging in test to try to understand where it stalls on Travis

--- account_invoice_factur-x/tests/test_facturx_invoice.py
+++ account_invoice_factur-x/tests/test_facturx_invoice.py
@@ -6,16 +6,23 @@ from odoo.addons.account_payment_unece.tests.test_account_invoice import \
     TestAccountInvoice
 from facturx import get_facturx_xml_from_pdf
 from lxml import etree
+import logging
+logger = logging.getLogger(__name__)
 
 
 class TestFacturXInvoice(TestAccountInvoice):
 
     def test_deep_customer_invoice(self):
         invoice = self.test_only_create_invoice()
+        logger.info('invoice.id=%s', invoice.id)
         pdf_content = self.env['report'].get_pdf(
             [invoice.id], 'account.report_invoice')
+        logger.info(
+            'len pdf_content=%s type=%s', len(pdf_content), type(pdf_content))
         res = get_facturx_xml_from_pdf(pdf_content, check_xsd=True)
+        logger.info('res=%s', res)
         self.assertTrue(res[0], 'factur-x.xml')
         xml_root = etree.fromstring(res[1])
+        logger.info('xml_root=%s', xml_root)
         self.assertTrue(xml_root.tag.startswith(
             '{urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100'))
