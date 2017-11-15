de6d4ac2aa7de32568fd1db3548635a882403913
Alexis de Lattre <alexis.delattre@akretion.com>
2017-11-16 00:13:29 +0100

    Factur-X invoice generation: level is now configurable
    Factur-X refund generation: type 380 or 381 is now a config option
    Fix some mistakes in Factur-X XML
    Update XSD to latest version

--- account_invoice_factur-x/tests/test_facturx_invoice.py
+++ account_invoice_factur-x/tests/test_facturx_invoice.py
@@ -4,15 +4,25 @@
 
 from odoo.addons.account_payment_unece.tests.test_account_invoice import \
     TestAccountInvoice
-from facturx import get_facturx_xml_from_pdf, check_facturx_xsd
+from facturx import get_facturx_xml_from_pdf, check_facturx_xsd,\
+    get_facturx_level
+from lxml import etree
 
 
 class TestFacturXInvoice(TestAccountInvoice):
 
     def test_deep_customer_invoice(self):
         invoice = self.test_only_create_invoice()
-        pdf_content = self.env['report'].get_pdf(
-            [invoice.id], 'account.report_invoice')
-        res = get_facturx_xml_from_pdf(pdf_content, check_xsd=True)
-        self.assertTrue(res[0], 'factur-x.xml')
-        self.assertTrue(check_facturx_xsd(res[1]))
+        for level in ['minimum', 'basicwl', 'basic', 'en16931']:
+            invoice.company_id.facturx_level = level
+            pdf_content = self.env['report'].get_pdf(
+                [invoice.id], 'account.report_invoice')
+            xml_filename, xml_string = get_facturx_xml_from_pdf(
+                pdf_content, check_xsd=True)
+            self.assertTrue(xml_filename, 'factur-x.xml')
+            xml_root = etree.fromstring(xml_string)
+            facturx_level = get_facturx_level(xml_root)
+            self.assertEqual(facturx_level, level)
+            self.assertTrue(
+                check_facturx_xsd(
+                    xml_root, flavor='factur-x', facturx_level=facturx_level))
