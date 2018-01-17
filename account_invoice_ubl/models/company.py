b4136ac39a5ddd8b180fee87c659db6f873a424e
Alexis de Lattre <alexis.delattre@akretion.com>
2018-01-17 21:15:48 +0100

    Add module account_e-invoice_generate to ensure compatibility for XML embedding in PDF between account_invoice_factur-x and account_invoice_ubl

--- account_invoice_ubl/models/company.py
+++ account_invoice_ubl/models/company.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2016-2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
@@ -8,6 +8,9 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    xml_format_in_pdf_invoice = fields.Selection(
+        selection_add=[('ubl', 'Universal Business Language (UBL)')],
+        default='ubl')
     embed_pdf_in_ubl_xml_invoice = fields.Boolean(
         string='Embed PDF in UBL XML Invoice',
         help="If active, the standalone UBL Invoice XML file will include the "
