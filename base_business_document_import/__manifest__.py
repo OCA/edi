e643d544ab177479fa8472b3d08d054b8de2d070
Nicolas JEUDY <nicolas@panda-chi.io>
2018-01-27 22:36:20 +0100

    base_business_document_import migrate to 11.0

--- base_business_document_import/__manifest__.py
+++ base_business_document_import/__manifest__.py
@@ -1,21 +1,19 @@
-# -*- coding: utf-8 -*-
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Business Document Import',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'license': 'AGPL-3',
     'summary': 'Provides technical tools to import sale orders or supplier '
     'invoices',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'author': 'Akretion, Nicolas JEUDY, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/edi',
     'depends': [
         'base_vat_sanitized',
         'account_tax_unece',
         'product_uom_unece',
-        ],
+    ],
     'external_dependencies': {'python': ['PyPDF2']},
-    'installable': True,
 }
