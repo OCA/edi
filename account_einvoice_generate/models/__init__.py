eabd56ca99c91fb05fb3c2ca24f6c49b640dc5e9
Andrea <a.stirpe@onestein.nl>
2018-02-06 13:55:38 +0100

    [11.0][MIG] account_e-invoice_generate to V11

--- account_einvoice_generate/models/__init__.py
+++ account_einvoice_generate/models/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import company
-from . import account_config_settings
+from . import res_company
+from . import res_config_settings
---
+++ account_einvoice_generate/models/res_config_settings.py
@@ -0,0 +1,12 @@
+# Copyright 2018 Akretion (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    xml_format_in_pdf_invoice = fields.Selection(
+        related='company_id.xml_format_in_pdf_invoice')
