50201aa27ad3199438942a8a6673fd78667be487
Andrea <a.stirpe@onestein.nl>
2018-02-09 14:38:36 +0100

    [11.0][MIG] account_invoice_ubl

--- account_invoice_ubl/post_install.py
+++ account_invoice_ubl/post_install.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2018 Akretion (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, SUPERUSER_ID
 
