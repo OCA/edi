7cf30ecc245d279f12147d16bbeb5899b15e58d4
Andrea <a.stirpe@onestein.nl>
2018-02-09 14:38:36 +0100

    [11.0][MIG] account_invoice_ubl

---
+++ account_invoice_ubl/models/res_config_settings.py
@@ -0,0 +1,12 @@
+# Copyright 2017 Akretion (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    embed_pdf_in_ubl_xml_invoice = fields.Boolean(
+        related='company_id.embed_pdf_in_ubl_xml_invoice')
