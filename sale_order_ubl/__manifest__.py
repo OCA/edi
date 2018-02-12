5533fc00c521f858c8a200efd3dcfc0d7d434701
Andrea <a.stirpe@onestein.nl>
2018-02-12 11:37:00 +0100

    [11.0][MIG] sale_order_ubl

--- sale_order_ubl/__manifest__.py
+++ sale_order_ubl/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Order UBL',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Embed UBL XML file inside the PDF quotation',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/edi/',
     'depends': ['sale', 'base_ubl'],
     'data': [],
     'installable': True,
