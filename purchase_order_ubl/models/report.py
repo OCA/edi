4a32eed65484b004c4bb94dd68f349be44dd2c6c
Andrea <a.stirpe@onestein.nl>
2018-02-13 09:14:05 +0100

    [11.0][MIG] purchase_order_ubl

--- purchase_order_ubl/models/report.py
+++ purchase_order_ubl/models/report.py
@@ -1,30 +1,26 @@
-# -*- coding: utf-8 -*-
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
-import logging
-
-logger = logging.getLogger(__name__)
+from odoo import api, models
 
 
 class Report(models.Model):
-    _inherit = 'report'
+    _inherit = "ir.actions.report"
 
-    @api.model
-    def get_pdf(self, docids, report_name, html=None, data=None):
+    @api.multi
+    def render_qweb_pdf(self, res_ids=None, data=None):
         """We go through that method when the PDF is generated for the 1st
         time and also when it is read from the attachment.
         This method is specific to QWeb"""
-        pdf_content = super(Report, self).get_pdf(
-            docids, report_name, html=html, data=data)
+        pdf_content = super(Report, self).render_qweb_pdf(res_ids, data)
         purchase_reports = [
             'purchase.report_purchaseorder',
             'purchase.report_purchasequotation']
         if (
-                report_name in purchase_reports and
-                len(docids) == 1 and
+                len(self) == 1 and
+                self.report_name in purchase_reports and
+                len(res_ids) == 1 and
                 not self._context.get('no_embedded_ubl_xml')):
-            order = self.env['purchase.order'].browse(docids[0])
+            order = self.env['purchase.order'].browse(res_ids[0])
             pdf_content = order.embed_ubl_xml_in_pdf(pdf_content=pdf_content)
         return pdf_content
