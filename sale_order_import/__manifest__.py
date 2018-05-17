9d3e6ef2e80fcc119bf8a8516a9e081f039eaeb3
tarteo <d.sluijk@onestein.nl>
2018-05-17 15:26:18 +0200

    [11.0][MIG] sale_order_import: Migration + add tests
    Add readme fragments

--- sale_order_import/__manifest__.py
+++ sale_order_import/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Order Import',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Import RFQ or sale orders from files',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/oca/edi',
     'depends': [
         'sale_commercial_partner',
         'base_business_document_import',
