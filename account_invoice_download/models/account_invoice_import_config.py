33210753ff89a37dc4bc443fbc346f7f6de824f0
Alexis de Lattre <alexis.delattre@akretion.com>
2018-08-15 00:49:15 +0200

    Add O2M field from import config to download config
    Code update and cleanup

---
+++ account_invoice_download/models/account_invoice_import_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountInvoiceImportConfig(models.Model):
+    _inherit = 'account.invoice.import.config'
+
+    download_config_ids = fields.One2many(
+        'account.invoice.download.config', 'import_config_id',
+        string='Download Configurations')
