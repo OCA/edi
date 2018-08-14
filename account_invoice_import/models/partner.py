3bb34ebcd89753dfdcc9a286f0b48cecf3036da7
Alexis de Lattre <alexis.delattre@akretion.com>
2018-08-14 23:48:34 +0200

    Add support for multiple invoice import configs in invoice import wizard
    Update other modules accordingly

--- account_invoice_import/models/partner.py
+++ account_invoice_import/models/partner.py
@@ -2,8 +2,7 @@
 # © 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, _
-from odoo.exceptions import UserError
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
