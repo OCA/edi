af3c7d58ead2d7b97dd965280e07514a932ca518
Alexis de Lattre <alexis.delattre@akretion.com>
2018-08-22 00:09:21 +0200

    Update headers to OCA standards
    Update website and category keys in manifest

--- account_invoice_download/__manifest__.py
+++ account_invoice_download/__manifest__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2017-2018 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Download',
     'version': '10.0.1.0.0',
-    'category': 'Accounting & Finance',
+    'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': 'Auto-download supplier invoices and import them',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/edi',
     'depends': [
         'account_invoice_import',
         ],
