822e6cc75405b5dcfc4a3b4923c96ccee6b2dc4c
Alexis de Lattre <alexis.delattre@akretion.com>
2018-08-23 23:48:28 +0200

    Add ability to run cron invoice download from admin for all companies in multi-company environnement
    Fix my previous commit on sequence field on invoice import config (caused a crash when creating a new record)
    Usability improvements on download config

--- base_business_document_import_phone/models/business_document_import.py
+++ base_business_document_import_phone/models/business_document_import.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2015-2018 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api
@@ -18,6 +19,7 @@ class BusinessDocumentImport(models.AbstractModel):
     def _hook_match_partner(
             self, partner_dict, chatter_msg, domain, partner_type_label):
         rpo = self.env['res.partner']
+        # 'domain' already contains the company_id criteria
         if partner_dict.get('country_code') and partner_dict.get('fax'):
             fax_num_e164 = False
             try:
@@ -28,9 +30,6 @@ class BusinessDocumentImport(models.AbstractModel):
             except:
                 pass
             logger.debug('_hook_match_partner fax_num_e164: %s', fax_num_e164)
-            domain = [
-                '|', ('company_id', '=', False),
-                ('company_id', '=', self.env.user.company_id.id)]
             if fax_num_e164:
                 partners = rpo.search(domain + [('fax', '=', fax_num_e164)])
                 if partners:
