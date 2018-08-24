aff6f315d5f3a1a0306781acd2392da8b60ec58a
Alexis de Lattre <alexis.delattre@akretion.com>
2018-08-24 23:47:18 +0200

    Add multi-company support in the invoice import mail gateway

--- account_invoice_import/models/company.py
+++ account_invoice_import/models/company.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2017-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
@@ -14,3 +15,12 @@ class ResCompany(models.Model):
     adjustment_debit_account_id = fields.Many2one(
         'account.account', string='Adjustment Debit Account',
         domain=[('deprecated', '=', False)])
+    invoice_import_email = fields.Char(
+        'Mail Gateway: Destination E-mail',
+        help="This field is used in multi-company setups to import the "
+        "invoices received by the mail gateway in the appropriate company")
+
+    _sql_constraints = [(
+        'invoice_import_email_uniq',
+        'unique(invoice_import_email)',
+        'This invoice import email already exists!')]
