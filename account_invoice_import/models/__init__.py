cb898a96901056a8c0999f3e82104ceb9a0d8ba8
Nicolas JEUDY <njeudy@panda-chi.io>
2018-10-02 13:43:00 +0200

    [FIX] clean rebase  from V10 that where wrong ...

--- account_invoice_import/models/__init__.py
+++ account_invoice_import/models/__init__.py
@@ -2,6 +2,6 @@
 
 from . import partner
 from . import company
-from . import account_config_settings
+from . import res_config_settings
 from . import account_invoice_import_config
 from . import account_invoice
