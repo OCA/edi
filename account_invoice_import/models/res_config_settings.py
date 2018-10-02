3c7ebca79738baf4b8a00863a8fa842bed3cc904
Nicolas JEUDY <njeudy@panda-chi.io>
2018-10-02 13:43:00 +0200

    [FIX] clean rebase  from V10 that where wrong ...

--- account_invoice_import/models/res_config_settings.py
+++ account_invoice_import/models/res_config_settings.py
@@ -11,3 +11,5 @@ class AccountConfigSettings(models.TransientModel):
         related='company_id.adjustment_credit_account_id')
     adjustment_debit_account_id = fields.Many2one(
         related='company_id.adjustment_debit_account_id')
+    invoice_import_email = fields.Char(
+        related='company_id.invoice_import_email')
