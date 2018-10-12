88df10694911efb2055eb5d12aca9e15c3e82f1b
Andrea <a.stirpe@onestein.nl>
2018-10-12 07:38:55 +0200

    Fix base64 encode/decode + remove supplier_invoice_number + code review

--- account_invoice_import/__manifest__.py
+++ account_invoice_import/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Account Invoice Import',
-    'version': '11.0.2.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Import supplier invoices/refunds as PDF or XML files',
