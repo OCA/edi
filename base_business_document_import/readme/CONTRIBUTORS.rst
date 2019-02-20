94ddf28a28c0f2b273a89df0da01ccafba0b97f8
Alexis de Lattre <alexis.delattre@akretion.com>
2019-02-21 00:15:39 +0100

    [MIG] base_business_document_import from 11 to 12
    Migrate base_business_document_import_phone from v10 to v12
    Convert readme to the new format

---
+++ base_business_document_import/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Alexis de Lattre <alexis.delattre@akretion.com>
+* Nicolas JEUDY <https://github.com/njeudy>
--- base_business_document_import_phone/__manifest__.py
+++ base_business_document_import_phone/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Business Document Import Phone',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Use phone numbers to match partners upon import of '
                'business documents',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/edi',
     'depends': [
         'base_phone',
         'base_business_document_import',
