6d733808ee305160add2b909982f35ffcb15f375
Alexis de Lattre <alexis.delattre@akretion.com>
2019-02-21 00:15:39 +0100

    [MIG] base_business_document_import from 11 to 12
    Migrate base_business_document_import_phone from v10 to v12
    Convert readme to the new format

--- base_business_document_import/__manifest__.py
+++ base_business_document_import/__manifest__.py
@@ -1,9 +1,10 @@
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Business Document Import',
-    'version': '11.0.1.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
     'license': 'AGPL-3',
     'summary': 'Provides technical tools to import sale orders or supplier '
