48d00ba5bf9223588deff07bf55de98dc55758f0
Denis Roussel <denis.roussel@acsone.eu>
2019-03-08 08:58:47 +0100

    [11.0][MIG] account_invoice_download

--- account_invoice_download/__manifest__.py
+++ account_invoice_download/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-2018 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Download',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
+    'development_status': 'Production/Stable',
     'summary': 'Auto-download supplier invoices and import them',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/edi',
