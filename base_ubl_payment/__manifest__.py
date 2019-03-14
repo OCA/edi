e27e656f3bbc65b9ac93645a14da8e52233d6b8e
mreficent <miquel.raich@eficent.com>
2019-03-14 14:42:27 +0100

    [MIG] base_ubl_payment: Migration to 12.0

--- base_ubl_payment/__manifest__.py
+++ base_ubl_payment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Base UBL Payment',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Hidden',
     'license': 'AGPL-3',
     'summary': 'Payment-related code for Universal Business Language (UBL)',
