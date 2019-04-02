f8889ddb63fc5414c95d002a81c0c2025d96f0d0
Andrea <a.stirpe@onestein.nl>
2019-04-02 10:16:52 +0200

    [12.0][MIG] account_invoice_ubl

--- account_invoice_ubl/models/ir_actions_report.py
+++ account_invoice_ubl/models/ir_actions_report.py
@@ -2,10 +2,7 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
-import logging
-
-logger = logging.getLogger(__name__)
+from odoo import api, models
 
 
 class IrActionsReport(models.Model):
@@ -16,8 +13,7 @@ class IrActionsReport(models.Model):
         """We go through that method when the PDF is generated for the 1st
         time and also when it is read from the attachment.
         This method is specific to QWeb"""
-        pdf_content = super(IrActionsReport, self).render_qweb_pdf(
-            res_ids, data)
+        pdf_content = super().render_qweb_pdf(res_ids, data)
         invoice_reports = self._get_invoice_reports_ubl()
         if (
                 len(self) == 1 and
