5e58da0e3bb8bd40745a05bdaf070e31903afcc1
Andrea <a.stirpe@onestein.nl>
2019-04-02 10:16:52 +0200

    [12.0][MIG] account_invoice_ubl

--- account_invoice_ubl/tests/test_ubl_generate.py
+++ account_invoice_ubl/tests/test_ubl_generate.py
@@ -2,7 +2,7 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.addons.account_payment_unece.tests.test_account_invoice import \
+from odoo.addons.account_tax_unece.tests.test_account_invoice import \
     TestAccountInvoice
 
 
@@ -15,8 +15,10 @@ class TestUblInvoice(TestAccountInvoice):
         if invoice.company_id.xml_format_in_pdf_invoice != 'ubl':
             invoice.company_id.xml_format_in_pdf_invoice = 'ubl'
         for version in ['2.0', '2.1']:
-            pdf_file = ro.with_context(ubl_version=version).render_qweb_pdf(
-                invoice.ids)[0]
+            pdf_file = ro.with_context(
+                ubl_version=version,
+                force_report_rendering=True
+            ).render_qweb_pdf(invoice.ids)[0]
             res = buo.get_xml_files_from_pdf(pdf_file)
             invoice_filename = invoice.get_ubl_filename(version=version)
             self.assertTrue(invoice_filename in res)
