c5073b7936cea7ed3c9a4affe6f9061441cb439e
Andrea <a.stirpe@onestein.nl>
2019-04-03 09:10:45 +0200

    [12.0][MIG] sale_order_import

--- sale_order_import/__manifest__.py
+++ sale_order_import/__manifest__.py
@@ -1,9 +1,9 @@
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Order Import',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Import RFQ or sale orders from files',
