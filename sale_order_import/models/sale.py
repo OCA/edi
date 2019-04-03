14324dcff060fd2319d762ec17fa0d46384394c9
Andrea <a.stirpe@onestein.nl>
2019-04-03 09:10:45 +0200

    [12.0][MIG] sale_order_import

--- sale_order_import/models/sale.py
+++ sale_order_import/models/sale.py
@@ -1,5 +1,5 @@
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models, _
 
--- sale_order_import/readme/CONTRIBUTORS.rst
+++ sale_order_import/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Alexis de Lattre <alexis.delattre@akretion.com>
 * Dennis Sluijk <d.sluijk@onestein.nl>
+* Andrea Stirpe <a.stirpe@onestein.nl>
