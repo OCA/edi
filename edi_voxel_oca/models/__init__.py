f240d72a88828878cb16b5219f43ac12d0decec5
ernesto <ernesto.tejeda87@gmail.com>
2019-04-09 12:19:56 -0400

    edi_voxel: new module

---
+++ edi_voxel_oca/models/__init__.py
@@ -0,0 +1,8 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import res_company
+from . import res_config_settings
+from . import voxel_mixin
+from . import product_uom
+from . import account
+from . import queue_job
