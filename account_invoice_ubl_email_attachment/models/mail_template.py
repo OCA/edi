2ffa011a7a3cf231af8def846378b4f6691e533f
Andrea <a.stirpe@onestein.nl>
2019-05-03 10:53:06 +0200

    Add check for robustness

--- account_invoice_ubl_email_attachment/models/mail_template.py
+++ account_invoice_ubl_email_attachment/models/mail_template.py
@@ -23,7 +23,7 @@ class MailTemplate(models.Model):
             ])
             if not ubl_attachments:
                 ubl_attachments = invoice._generate_email_ubl_attachment()
-            if len(ubl_attachments) == 1:
+            if len(ubl_attachments) == 1 and template.report_name:
                 report_name = self.render_template(
                     template.report_name, template.model, res_id)
                 ext = '.xml'
