f9cd74bb6de48de92af5b28bc3cb6595358a04c9
Andrea <a.stirpe@onestein.nl>
2019-12-10 14:36:58 +0100

    pre-commit

--- account_einvoice_generate/__manifest__.py
+++ account_einvoice_generate/__manifest__.py
@@ -3,17 +3,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account e-invoice Generate',
-    'version': '13.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'license': 'AGPL-3',
-    'summary': 'Technical module to generate PDF invoices with '
-               'embedded XML file',
-    'author': 'Akretion,Onestein,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/edi',
-    'depends': ['account'],
-    'data': [
-        'views/res_config_settings.xml',
-    ],
-    'installable': True,
+    "name": "Account e-invoice Generate",
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "summary": "Technical module to generate PDF invoices with " "embedded XML file",
+    "author": "Akretion,Onestein,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["account"],
+    "data": ["views/res_config_settings.xml"],
+    "installable": True,
 }
