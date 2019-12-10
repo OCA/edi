2b6f3c1e366c9c1185bc06db64719d65e678ae90
Andrea <a.stirpe@onestein.nl>
2019-12-10 14:36:58 +0100

    pre-commit

--- account_e-invoice_generate/models/res_company.py
+++ account_e-invoice_generate/models/res_company.py
@@ -6,8 +6,8 @@ from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
-    xml_format_in_pdf_invoice = fields.Selection([
-        ('none', 'None'),
-    ], string='XML Format embedded in PDF invoice')
+    xml_format_in_pdf_invoice = fields.Selection(
+        [("none", "None")], string="XML Format embedded in PDF invoice"
+    )
