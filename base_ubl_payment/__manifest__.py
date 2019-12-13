5c4578623c5761275675a284b4b42433dd10daba
Andrea <a.stirpe@onestein.nl>
2019-12-13 12:21:47 +0100

    [13.0][MIG] base_ubl_payment

--- base_ubl_payment/__manifest__.py
+++ base_ubl_payment/__manifest__.py
@@ -2,16 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base UBL Payment',
-    'version': '12.0.1.0.0',
-    'category': 'Hidden',
-    'license': 'AGPL-3',
-    'summary': 'Payment-related code for Universal Business Language (UBL)',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/oca/edi/',
-    'depends': [
-        'account_payment_unece',
-        'base_ubl',
-        ],
-    'installable': True,
+    "name": "Base UBL Payment",
+    "version": "13.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "summary": "Payment-related code for Universal Business Language (UBL)",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/oca/edi/",
+    "depends": ["account_payment_unece", "base_ubl"],
+    "external_dependencies": {"python": ["lxml"]},
+    "installable": True,
 }
