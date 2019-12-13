8ff09bfa12ecab19ed262f4716a3d9dcccbc795e
Andrea <a.stirpe@onestein.nl>
2019-12-13 13:14:40 +0100

    [13.0][MIG] account_invoice_ubl

--- account_invoice_ubl/hooks.py
+++ account_invoice_ubl/hooks.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Akretion (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, SUPERUSER_ID
@@ -10,4 +11,12 @@ def set_xml_format_in_pdf_invoice_to_ubl(cr, registry):
         env = api.Environment(cr, SUPERUSER_ID, {})
         companies = env['res.company'].search([])
         companies.write({'xml_format_in_pdf_invoice': 'ubl'})
-    return
+
+
+def remove_ubl_xml_format_in_pdf_invoice(cr, registry):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        companies = env['res.company'].search([
+            ('xml_format_in_pdf_invoice', '=', 'ubl')
+        ])
+        companies.write({'xml_format_in_pdf_invoice': 'none'})
