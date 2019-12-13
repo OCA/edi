9b2a22ee78d4e456860a9ed45f6635fa9d90bd5c
Andrea <a.stirpe@onestein.nl>
2019-12-13 11:49:21 +0100

    pre-commit

--- base_ubl/__manifest__.py
+++ base_ubl/__manifest__.py
@@ -2,18 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base UBL',
-    'version': '13.0.1.0.0',
-    'category': 'Hidden',
-    'license': 'AGPL-3',
-    'summary': 'Base module for Universal Business Language (UBL)',
-    'author': 'Akretion,Onestein,Odoo Community Association (OCA)',
-    'website': 'https://github.com/oca/edi/',
-    'depends': [
-        'uom_unece',
-        'account_tax_unece',
-        'base_vat_sanitized',
-        ],
-    'external_dependencies': {'python': ['PyPDF2']},
-    'installable': True,
+    "name": "Base UBL",
+    "version": "13.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "summary": "Base module for Universal Business Language (UBL)",
+    "author": "Akretion,Onestein,Odoo Community Association (OCA)",
+    "website": "https://github.com/oca/edi/",
+    "depends": ["uom_unece", "account_tax_unece", "base_vat_sanitized"],
+    "external_dependencies": {"python": ["PyPDF2"]},
+    "installable": True,
 }
