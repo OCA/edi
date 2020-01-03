a26701538a7b1036276aac466bbf19dfa63363df
Andrea <a.stirpe@onestein.nl>
2020-01-03 10:26:21 +0100

    [12.0][MIG] account_invoice_import

--- account_invoice_import/models/res_config_settings.py
+++ account_invoice_import/models/res_config_settings.py
@@ -8,8 +8,8 @@ class AccountConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     adjustment_credit_account_id = fields.Many2one(
-        related='company_id.adjustment_credit_account_id')
+        related='company_id.adjustment_credit_account_id', readonly=False)
     adjustment_debit_account_id = fields.Many2one(
-        related='company_id.adjustment_debit_account_id')
+        related='company_id.adjustment_debit_account_id', readonly=False)
     invoice_import_email = fields.Char(
-        related='company_id.invoice_import_email')
+        related='company_id.invoice_import_email', readonly=False)
