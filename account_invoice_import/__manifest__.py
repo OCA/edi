d4630c5a7f6b1bca8ad8c4b186a96bd0fe3fc9b9
Andrea <a.stirpe@onestein.nl>
2020-01-03 10:26:21 +0100

    [12.0][MIG] account_invoice_import

--- account_invoice_import/__manifest__.py
+++ account_invoice_import/__manifest__.py
@@ -1,22 +1,19 @@
 # © 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Import',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Import supplier invoices/refunds as PDF or XML files',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/edi',
     'depends': [
         'account',
         'base_iban',
         'base_business_document_import',
         'onchange_helper',
-        # add bank_payment dependency to have commercial_partner_id on invoice
-        # PR in odoo will be create soon
-        'account_payment_partner',
         ],
     'data': [
         'security/ir.model.access.csv',
