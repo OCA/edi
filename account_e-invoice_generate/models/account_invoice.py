2c1b4d81e279545f8ba21b93a0ac125abe3dfb4b
Alexis de Lattre <alexis.delattre@akretion.com>
2020-02-11 00:19:28 +0100

    account_e-invoice_generate: Add method _get_invoice_report_names()

--- account_e-invoice_generate/models/account_invoice.py
+++ account_e-invoice_generate/models/account_invoice.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountInvoice(models.Model):
@@ -11,3 +11,9 @@ class AccountInvoice(models.Model):
         """This method is designed to be inherited in localization modules"""
         self.ensure_one()
         return None
+
+    @api.model
+    def _get_invoice_report_names(self):
+        return [
+            'account.report_invoice',
+            'account.report_invoice_with_payments']
