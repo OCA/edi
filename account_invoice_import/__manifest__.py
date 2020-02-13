03779a92ee685cb190a373fb5a7016ba25e40e39
Alexis de Lattre <alexis.delattre@akretion.com>
2020-02-13 14:45:49 +0100

    account_invoice_download: start OCA import bill wizard when using the "Import Bill" button on journal dashboard

--- account_invoice_import/__manifest__.py
+++ account_invoice_import/__manifest__.py
@@ -22,6 +22,7 @@
         'views/res_config_settings.xml',
         'wizard/account_invoice_import_view.xml',
         'views/account_invoice.xml',
+        'views/account_journal_dashboard.xml',
         'views/partner.xml',
     ],
     'images': ['images/sshot-wizard1.png'],
