162921d978c86871a92eb90c0f886cfa4f41f90b
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-14 16:32:54 +0200

    [MIG] account_invoice_import_facturx to v12
    Update technical name of module to remove '-'
    Up-port PR #78 Auto-creation of bank accounts upon invoice import is now optional
    Fix returned action when using the import wizard

--- account_invoice_import/models/company.py
+++ account_invoice_import/models/company.py
@@ -18,6 +18,8 @@ class ResCompany(models.Model):
         'Mail Gateway: Destination E-mail',
         help="This field is used in multi-company setups to import the "
         "invoices received by the mail gateway in the appropriate company")
+    invoice_import_create_bank_account = fields.Boolean(
+        string='Auto-create Bank Account of Supplier')
 
     _sql_constraints = [(
         'invoice_import_email_uniq',
