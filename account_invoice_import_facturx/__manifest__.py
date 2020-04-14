73bc477d4897fc1d233345248c7ff187e8eb8e63
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-14 16:32:54 +0200

    [MIG] account_invoice_import_facturx to v12
    Update technical name of module to remove '-'
    Up-port PR #78 Auto-creation of bank accounts upon invoice import is now optional
    Fix returned action when using the import wizard

---
+++ account_invoice_import_facturx/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2015-2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Invoice Import Factur-X',
+    'version': '12.0.1.0.0',
+    'category': 'Invoicing Management',
+    'license': 'AGPL-3',
+    'summary': 'Import Factur-X/ZUGFeRD supplier invoices/refunds',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/edi',
+    'depends': ['account_invoice_import', 'base_facturx'],
+    'external_dependencies': {'python': ['facturx']},
+    'data': ['wizard/account_invoice_import_view.xml'],
+    'demo': ['demo/demo_data.xml'],
+    'installable': True,
+}
