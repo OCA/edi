888e715d83312800c2f1c990998986ae83996c53
Cyril VINH-TUNG <cyril@invitu.com>
2020-04-24 22:24:32 -1000

    [FIX] fix purchase_order_ubl after reporting engine improvement #919275edef5f5e206954cc6159217dc4e4cdfd67

--- purchase_order_ubl/models/report.py
+++ purchase_order_ubl/models/report.py
@@ -8,20 +8,21 @@ class Report(models.Model):
     _inherit = "ir.actions.report"
 
     @api.multi
-    def render_qweb_pdf(self, res_ids=None, data=None):
+    def _post_pdf(self, save_in_attachment, pdf_content=None, res_ids=None):
         """We go through that method when the PDF is generated for the 1st
         time and also when it is read from the attachment.
         This method is specific to QWeb"""
-        pdf_content = super(Report, self).render_qweb_pdf(res_ids, data)
         purchase_reports = self._get_purchase_order_ubl_reports()
         if (
                 len(self) == 1 and
                 self.report_name in purchase_reports and
+                res_ids and
                 len(res_ids) == 1 and
                 not self._context.get('no_embedded_ubl_xml')):
             order = self.env['purchase.order'].browse(res_ids[0])
             pdf_content = order.embed_ubl_xml_in_pdf(pdf_content=pdf_content)
-        return pdf_content
+        return super()._post_pdf(
+            save_in_attachment, pdf_content=pdf_content, res_ids=res_ids)
 
     def _get_purchase_order_ubl_reports(self):
         return [
