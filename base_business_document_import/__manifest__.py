573866d07cc9ca0db26fd6a9c846654f91428cb8
sebalix <seb@usr-src.org>
2020-05-13 16:04:25 +0200

    [IMP] base_business_document_import: black, isort, prettier

--- base_business_document_import/__manifest__.py
+++ base_business_document_import/__manifest__.py
@@ -3,18 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base Business Document Import',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'license': 'AGPL-3',
-    'summary': 'Provides technical tools to import sale orders or supplier '
-    'invoices',
-    'author': 'Akretion, Nicolas JEUDY, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/edi',
-    'depends': [
-        'base_vat_sanitized',
-        'account_tax_unece',
-        'uom_unece',
-    ],
-    'external_dependencies': {'python': ['PyPDF2']},
+    "name": "Base Business Document Import",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Provides technical tools to import sale orders or supplier invoices",
+    "author": "Akretion, Nicolas JEUDY, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["base_vat_sanitized", "account_tax_unece", "uom_unece"],
+    "external_dependencies": {"python": ["PyPDF2"]},
 }
