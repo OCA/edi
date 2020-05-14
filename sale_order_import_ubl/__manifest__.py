992b6bc93fb263fa112e0b6ba6d296d80b57cedd
sebalix <seb@usr-src.org>
2020-05-14 15:05:20 +0200

    [IMP] sale_order_import_ubl: black, isort, prettier

--- sale_order_import_ubl/__manifest__.py
+++ sale_order_import_ubl/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Sale Order UBL Import',
-    'version': '12.0.1.0.0',
-    'category': 'Sales Management',
-    'license': 'AGPL-3',
-    'summary': 'Import UBL XML sale order files',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/oca/edi',
-    'depends': ['sale_order_import', 'base_ubl'],
-    'demo': ['demo/demo_data.xml'],
-    'installable': True,
+    "name": "Sale Order UBL Import",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "summary": "Import UBL XML sale order files",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/oca/edi",
+    "depends": ["sale_order_import", "base_ubl"],
+    "demo": ["demo/demo_data.xml"],
+    "installable": True,
 }
