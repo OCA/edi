d1e15d9b568a6c4a3fd9a854dca286995f813a5f
sebalix <seb@usr-src.org>
2020-05-14 15:06:03 +0200

    [MIG] sale_order_import_ubl: Migration to 13.0

--- sale_order_import_ubl/__manifest__.py
+++ sale_order_import_ubl/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order UBL Import",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "summary": "Import UBL XML sale order files",
