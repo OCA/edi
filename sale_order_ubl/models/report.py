ad90a6372003d775b90a418919ae127f4c138e92
Andrea <a.stirpe@onestein.nl>
2020-05-14 08:23:22 +0200

    pre-commit

--- sale_order_ubl/models/report.py
+++ sale_order_ubl/models/report.py
@@ -14,10 +14,11 @@ class IrActionsReport(models.Model):
         This method is specific to QWeb"""
         pdf_content = super().render_qweb_pdf(res_ids, data)
         if (
-                len(self) == 1 and
-                self.report_name == 'sale.report_saleorder' and
-                len(res_ids) == 1 and
-                not self._context.get('no_embedded_ubl_xml')):
-            order = self.env['sale.order'].browse(res_ids[0])
+            len(self) == 1
+            and self.report_name == "sale.report_saleorder"
+            and len(res_ids) == 1
+            and not self._context.get("no_embedded_ubl_xml")
+        ):
+            order = self.env["sale.order"].browse(res_ids[0])
             pdf_content = order.embed_ubl_xml_in_pdf(pdf_content=pdf_content)
         return pdf_content
