542b82c8f0dff25afe32a036e6834c87fd403abe
Nicolas JEUDY <njeudy@myceliandre.fr>
2020-05-18 18:04:39 +0200

    [MIG] migrate account_invoice_download and account_invoice_download_ovh to V12.0

--- account_invoice_download_ovh/__manifest__.py
+++ account_invoice_download_ovh/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Download OVH',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': 'Get OVH Invoice via the API',
