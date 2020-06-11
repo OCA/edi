f5720dadb564a19bbceda2be55467d04faa53985
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-11 15:55:02 +0200

    base_facturx: black, isort, ...

--- base_facturx/__manifest__.py
+++ base_facturx/__manifest__.py
@@ -3,19 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base Factur-X',
-    'version': '13.0.1.0.0',
-    'category': 'Invoicing Management',
-    'license': 'AGPL-3',
-    'summary': 'Base module for Factur-X/ZUGFeRD',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/edi',
-    'depends': [
-        'uom_unece',
-        'account_tax_unece',
-        'account_payment_unece',
-        ],
-    'data': ['data/zugferd_codes.xml'],
-    'installable': True,
+    "name": "Base Factur-X",
+    "version": "13.0.1.0.0",
+    "category": "Invoicing Management",
+    "license": "AGPL-3",
+    "summary": "Base module for Factur-X/ZUGFeRD",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/edi",
+    "depends": ["uom_unece", "account_tax_unece", "account_payment_unece"],
+    "data": ["data/zugferd_codes.xml"],
+    "installable": True,
 }
