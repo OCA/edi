69a1081a2b5c89ea2d7ae609a9518a27a8909176
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-06-16 10:07:14 +0200

    [13.0][ADD] account_invoice_export

---
+++ account_invoice_export/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Account Invoice Export",
+    "version": "13.0.1.0.0",
+    "category": "Invoicing Management",
+    "license": "AGPL-3",
+    "summary": "",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "website": "https://github.com/oca/edi",
+    "depends": ["account_invoice_transmit_method", "queue_job"],
+    "data": [
+        "data/mail_activity_type.xml",
+        "views/transmit_method.xml",
+        "views/account_move.xml",
+        "views/message_template.xml",
+    ],
+    "installable": True,
+}
