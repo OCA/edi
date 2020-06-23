f672df41f0af9c7059afda6e410e1088b8addb25
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-23 12:24:13 +0200

    [IMP] edi_voxel: Look product customer code also in commercial partner (squashed)
    [FIX] edi_voxel: Trailing comma
    
    [FIX] pre-commit

--- edi_voxel/models/voxel_mixin.py
+++ edi_voxel/models/voxel_mixin.py
@@ -187,16 +187,22 @@ class VoxelMixin(models.AbstractModel):
         return self.env["voxel.login"]
 
     def _get_customer_product_sku(self, product, partner):
+        """Look for an entry for specific contact (sending/invoicing contact),
+        and if not found, look for the commercial partner.
+        """
+        domain = [
+            "|",
+            ("product_id", "=", product.id),
+            "&",
+            ("product_tmpl_id", "=", product.product_tmpl_id.id),
+            ("product_id", "=", False),
+        ]
         customerinfo = self.env["product.customerinfo"].search(
-            [
-                ("name", "=", partner.id),
-                "|",
-                ("product_id", "=", product.id),
-                "&",
-                ("product_tmpl_id", "=", product.product_tmpl_id.id),
-                ("product_id", "=", False),
-            ],
-            limit=1,
-            order="product_id, sequence",
+            [("name", "=", partner.id)] + domain, order="product_id, sequence",
         )
-        return customerinfo.product_code
+        if not customerinfo:
+            customerinfo = self.env["product.customerinfo"].search(
+                [("name", "=", partner.commercial_partner_id.id)] + domain,
+                order="product_id, sequence",
+            )
+        return customerinfo[:1].product_code
