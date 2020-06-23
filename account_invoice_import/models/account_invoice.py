5c35ea921103f84f3d683c9eab08e1282b456a77
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2020-06-23 09:20:08 +0200

    [IMP] account_invoice_import: black, isort, prettier

--- account_invoice_import/models/account_invoice.py
+++ account_invoice_import/models/account_invoice.py
@@ -1,25 +1,27 @@
 # © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, api, _
+from odoo import _, api, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     @api.multi
     def name_get(self):
         """Add amount_untaxed in name_get of invoices"""
         res = super().name_get()
-        if self.env.context.get('invoice_show_amount'):
+        if self.env.context.get("invoice_show_amount"):
             new_res = []
             for (inv_id, name) in res:
                 inv = self.browse(inv_id)
                 # I didn't find a python method to easily display
                 # a float + currency symbol (before or after)
                 # depending on lang of context and currency
-                name += _(' Amount w/o tax: %s %s') % (
-                    inv.amount_untaxed, inv.currency_id.name)
+                name += _(" Amount w/o tax: %s %s") % (
+                    inv.amount_untaxed,
+                    inv.currency_id.name,
+                )
                 new_res.append((inv_id, name))
             return new_res
         return res
--- account_invoice_import/models/partner.py
+++ account_invoice_import/models/partner.py
@@ -5,21 +5,26 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     invoice_import_ids = fields.One2many(
-        'account.invoice.import.config', 'partner_id',
-        string='Invoice Import Configuration')
+        "account.invoice.import.config",
+        "partner_id",
+        string="Invoice Import Configuration",
+    )
     invoice_import_count = fields.Integer(
-        compute='_compute_invoice_import_count',
-        string='Number of Invoice Import Configurations',
-        readonly=True)
+        compute="_compute_invoice_import_count",
+        string="Number of Invoice Import Configurations",
+        readonly=True,
+    )
 
     def _compute_invoice_import_count(self):
-        config_data = self.env['account.invoice.import.config'].read_group(
-            [('partner_id', 'in', self.ids)], ['partner_id'], ['partner_id'])
-        mapped_data = dict([
-            (config['partner_id'][0], config['partner_id_count'])
-            for config in config_data])
+        config_data = self.env["account.invoice.import.config"].read_group(
+            [("partner_id", "in", self.ids)], ["partner_id"], ["partner_id"]
+        )
+        mapped_data = {
+                config["partner_id"][0]: config["partner_id_count"]
+                for config in config_data
+        }
         for partner in self:
             partner.invoice_import_count = mapped_data.get(partner.id, 0)
