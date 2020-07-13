418b8e6a3eee0b9fe99a5b71fb324a1b163ad5a0
David Beal <david.beal@akretion.com>
2020-07-13 19:25:24 +0200

    FIX purchase_order_import_*: version number after manual merge

--- purchase_order_import/__manifest__.py
+++ purchase_order_import/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Purchase Order Import',
-    'version': '10.0.1.0.0',
+    'version': '10.0.2.0.0',
     'category': 'Purchase Management',
     'license': 'AGPL-3',
     'summary': 'Update RFQ via the import of quotations from suppliers',
