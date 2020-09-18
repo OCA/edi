063e3350c8923e1ded3b4a0343106914cf7a2953
Jacques-Etienne Baudoux <je@bcim.be>
2020-09-18 13:54:50 +0200

    Allow to make parsing hook based on PartyIdentification/ID
    Some industries still identify party with a GLN and schemeID="GLN"
    or with a VAT number and schemeID="<country code>:VAT". This allow to
    process it with a parsing hook in the partner matching.
    
    Add module partner_identification_import to match partner by external
    identifier using the above hook.
    
    fixup! Allow to make parsing hook based on PartyIdentification/ID

---
+++ partner_identification_import/models/business_document_import.py
@@ -0,0 +1,43 @@
+# Copyright 2020 Jacques-Etienne Baudoux <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, models
+
+
+class BusinessDocumentImport(models.AbstractModel):
+    _inherit = "business.document.import"
+
+    @api.model
+    def _hook_match_partner(
+        self, partner_dict, chatter_msg, domain, partner_type_label
+    ):
+        if partner_dict.get("id_number") and partner_dict.get("id_schemeID"):
+            categ = self.env["res.partner.id_category"].search(
+                [("code", "=", partner_dict.get("id_schemeID"))]
+            )
+            if categ:
+                id_number = self.env["res.partner.id_number"].search(
+                    [
+                        ("category_id", "=", categ.id),
+                        ("name", "=", partner_dict["id_number"]),
+                        ("status", "!=", "close"),
+                    ],
+                    limit=1,
+                )
+                if id_number:
+                    return id_number.partner_id
+                raise self.user_error_wrap(
+                    _(
+                        "Odoo couldn't find a partner corresponding to the "
+                        "following information extracted from the business document:\n"
+                        "ID Number: %s\n"
+                        "ID Number Category: %s\n"
+                    )
+                    % (
+                        partner_dict["id_number"],
+                        partner_dict["id_schemeID"],
+                   )
+                )
+        return super()._hook_match_partner(
+            partner_dict, chatter_msg, domain, partner_type_label
+        )
