6c05e445789c58f84ce4ee3e1f47d2bae762a0e6
Enric Tobella <etobella@creublanca.es>
2020-11-21 13:05:20 +0100

    [IMP] edi: Set Send function
    Co-Authored-By: Simone Orsi <simahawk@gmail.com>

--- edi_oca/tests/test_edi_backend.py
+++ edi_oca/tests/test_edi_backend.py
@@ -2,16 +2,20 @@
 # @author: Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import base64
+
 import mock
 import psycopg2
 from freezegun import freeze_time
 
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
+from odoo.tests.common import tagged
 from odoo.tools import mute_logger
 
 from .common import EDIBackendCommonTestCase
 
 
+@tagged("-at_install", "post_install")
 class EDIBackendTestCase(EDIBackendCommonTestCase):
     def test_type_code(self):
         btype = self.backend_type_model.create(
@@ -63,3 +67,54 @@ class EDIBackendTestCase(EDIBackendCommonTestCase):
             mocked.return_value = "Any string"
             self.backend.generate_output(record)
             mocked.assert_called_with(record)
+
+    def test_send_record(self):
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+            "edi_exchange_state": "output_pending",
+            "exchange_file": base64.b64encode(b"1234"),
+        }
+        record = self.backend.create_record("test_csv_output", vals)
+        self.assertFalse(record.exchanged_on)
+        with mock.patch.object(type(self.backend), "_exchange_send") as patch:
+            patch.return_value = "AAA"
+            record.action_exchange_send()
+            patch.assert_called()
+        self.assertEqual(record.edi_exchange_state, "output_sent")
+        self.assertTrue(record.exchanged_on)
+
+    def test_send_record_with_error(self):
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+            "edi_exchange_state": "output_pending",
+            "exchange_file": base64.b64encode(b"1234"),
+        }
+        record = self.backend.create_record("test_csv_output", vals)
+        record.action_exchange_send()
+        self.assertEqual(record.edi_exchange_state, "output_error_on_send")
+
+    def test_send_inbound_error(self):
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+        }
+        record = self.backend.create_record("test_csv_input", vals)
+        with mock.patch.object(type(self.backend), "_exchange_send") as patch:
+            patch.return_value = "AAA"
+            with self.assertRaises(UserError):
+                record.action_exchange_send()
+            patch.assert_not_called()
+
+    def test_send_not_generated_record(self):
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+        }
+        record = self.backend.create_record("test_csv_output", vals)
+        with mock.patch.object(type(self.backend), "_exchange_send") as patch:
+            patch.return_value = "AAA"
+            with self.assertRaises(UserError):
+                record.action_exchange_send()
+            patch.assert_not_called()
