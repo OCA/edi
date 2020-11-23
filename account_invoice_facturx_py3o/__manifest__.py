7367d3aedc01d3784842533fcecd778838e2f198
Alexis de Lattre <alexis.delattre@akretion.com>
2020-11-23 11:56:23 +0100

    account_invoice_facturx/_py3o: black reformatting

--- account_invoice_facturx_py3o/__manifest__.py
+++ account_invoice_facturx_py3o/__manifest__.py
@@ -3,15 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Py3o Factur-x Invoice',
-    'version': '13.0.1.0.0',
-    'category': 'Invoicing Management',
-    'license': 'AGPL-3',
-    'summary': 'Generate Factur-X invoices with Py3o reporting engine',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/edi',
-    'depends': ['account_invoice_facturx', 'report_py3o'],
-    'external_dependencies': {'python': ['factur-x']},
-    'installable': True,
+    "name": "Py3o Factur-x Invoice",
+    "version": "13.0.1.0.0",
+    "category": "Invoicing Management",
+    "license": "AGPL-3",
+    "summary": "Generate Factur-X invoices with Py3o reporting engine",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/edi",
+    "depends": ["account_invoice_facturx", "report_py3o"],
+    "external_dependencies": {"python": ["factur-x"]},
+    "installable": True,
 }
