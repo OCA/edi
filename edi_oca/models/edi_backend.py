5fb6b03b1c530ce13db4350d041947395491eb04
Simone Orsi <simahawk@gmail.com>
2020-11-23 07:52:46 +0100

    edi: small fix/imp on models

--- edi_oca/models/edi_backend.py
+++ edi_oca/models/edi_backend.py
@@ -51,9 +51,77 @@ class EDIBackend(models.Model):
         :return: edi.exchange.record record
         """
         self.ensure_one()
+        _values = self._create_record_prepare_values(type_code, values)
+        return self.env["edi.exchange.record"].create(_values)
+
+    def _create_record_prepare_values(self, type_code, values):
+        res = values.copy()  # do not pollute original dict
         export_type = self.env["edi.exchange.type"].search(
-            [("code", "=", type_code), ("backend_id", "=", self.id)], limit=1
+            self._get_exchange_type_domain(type_code), limit=1
         )
         export_type.ensure_one()
-        values["type_id"] = export_type.id
-        return self.env["edi.exchange.record"].create(values)
+        res["type_id"] = export_type.id
+        res["backend_id"] = self.id
+        return res
+<<<<<<< HEAD
+=======
+
+    def _get_exchange_type_domain(self, code):
+        return [
+            ("code", "=", code),
+            "|",
+            ("backend_type_id", "=", self.backend_type_id.id),
+            ("backend_id", "=", self.id),
+        ]
+
+    def generate_output(self, exchange_record, store=True, force=False, **kw):
+        """Generate output content for given exchange record.
+
+        :param exchange_record: edi.exchange.record recordset
+        :param store: store output on the record itself
+        :param force: allow to re-genetate the content
+        :param kw: keyword args to be propagated to output generate handler
+        """
+        self.ensure_one()
+        self._validate_generate_output(exchange_record, force=force)
+        output = self._generate_output(exchange_record, **kw)
+        if output and store:
+            if not isinstance(output, bytes):
+                output = output.encode()
+            exchange_record.update(
+                {
+                    "exchange_file": base64.b64encode(output),
+                    "edi_exchange_state": "output_pending",
+                }
+            )
+        return tools.pycompat.to_text(output)
+
+    def _validate_generate_output(self, exchange_record, force=False):
+        exchange_record.ensure_one()
+        if (
+            exchange_record.edi_exchange_state != "new"
+            and exchange_record.exchange_file
+            and not force
+        ):
+            raise exceptions.UserError(
+                _(
+                    "Exchange record ID=%d is not in draft state "
+                    "and has already an output value."
+                )
+                % exchange_record.id
+            )
+        if not exchange_record.direction != "outbound":
+            raise exceptions.UserError(
+                _("Exchange record ID=%d is not file is not meant to b generated")
+                % exchange_record.id
+            )
+        if exchange_record.exchange_file:
+            raise exceptions.UserError(
+                _("Exchabge record ID=%d already has a file to process!")
+                % exchange_record.id
+            )
+
+    def _generate_output(self, exchange_record, **kw):
+        """To be implemented"""
+        raise NotImplementedError()
+>>>>>>> 6df23cc... fixup! edi: small fix/imp on models
--- edi_oca/models/edi_exchange_type.py
+++ edi_oca/models/edi_exchange_type.py
@@ -2,7 +2,7 @@
 # @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, exceptions, fields, models
 
 from odoo.addons.http_routing.models.ir_http import slugify
 
@@ -16,10 +16,13 @@ class EDIExchangeType(models.Model):
     _description = "EDI Exchange Type"
 
     backend_id = fields.Many2one(
-        string="EDI backend",
-        comodel_name="edi.backend",
+        string="EDI backend", comodel_name="edi.backend", ondelete="restrict",
+    )
+    backend_type_id = fields.Many2one(
+        string="EDI Backend type",
+        comodel_name="edi.backend.type",
         required=True,
-        ondelete="cascade",
+        ondelete="restrict",
     )
     name = fields.Char(required=True)
     code = fields.Char(required=True)
@@ -27,13 +30,30 @@ class EDIExchangeType(models.Model):
         selection=[("input", "Input"), ("output", "Output")], required=True
     )
     exchange_filename_pattern = fields.Char(default="{record_name}-{type.code}-{dt}")
-    exchange_file_ext = fields.Char(required=True)
+    # TODO make required if exchange_filename_pattern is
+    exchange_file_ext = fields.Char()
 
     ack_needed = fields.Boolean()
     ack_name = fields.Char()
     ack_code = fields.Char()
     ack_filename_pattern = fields.Char(default="{type.exchange_filename_pattern}.ack")
     ack_file_ext = fields.Char(default="")
+
+    _sql_constraints = [
+        (
+            "code_uniq",
+            "unique(code, backend_id)",
+            "The code must be unique per backend",
+        )
+    ]
+
+    @api.constrains("backend_id", "backend_type_id")
+    def _check_backend(self):
+        for rec in self:
+            if not rec.backend_id:
+                continue
+            if rec.backend_id.backend_type_id != rec.backend_type_id:
+                raise exceptions.UserError(_("Backend should respect backend type!"))
 
     def _make_exchange_filename(self, record, ack=False):
         """Generate filename."""
@@ -58,11 +78,3 @@ class EDIExchangeType(models.Model):
         if hasattr(record.record, "_get_edi_exchange_record_name"):
             return record.record._get_edi_exchange_record_name(record, self)
         return slugify(record.record.name)
-
-    _sql_constraints = [
-        (
-            "code_uniq",
-            "unique(code, backend_id)",
-            "The code must be unique per backend",
-        )
-    ]
