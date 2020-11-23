bdbd0d23f3499031b783d02fdca8656245e4344c
Alexis de Lattre <alexis.delattre@akretion.com>
2020-11-23 11:56:23 +0100

    account_invoice_facturx/_py3o: black reformatting

--- account_invoice_facturx/hooks.py
+++ account_invoice_facturx/hooks.py
@@ -2,20 +2,20 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def set_xml_format_in_pdf_invoice_to_facturx(cr, registry):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        companies = env['res.company'].search([])
-        companies.write({'xml_format_in_pdf_invoice': 'factur-x'})
+        companies = env["res.company"].search([])
+        companies.write({"xml_format_in_pdf_invoice": "factur-x"})
 
 
 def remove_facturx_xml_format_in_pdf_invoice(cr, registry):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        companies = env['res.company'].search([
-            ('xml_format_in_pdf_invoice', '=', 'factur-x')
-        ])
-        companies.write({'xml_format_in_pdf_invoice': 'none'})
+        companies = env["res.company"].search(
+            [("xml_format_in_pdf_invoice", "=", "factur-x")]
+        )
+        companies.write({"xml_format_in_pdf_invoice": "none"})
