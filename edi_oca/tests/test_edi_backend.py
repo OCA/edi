65bee9f33c4153034bc357d9de729444d0797d5f
Simone Orsi <simahawk@gmail.com>
2020-11-23 07:55:12 +0100

    edi: use freezegun for tests

--- edi_oca/tests/test_edi_backend.py
+++ edi_oca/tests/test_edi_backend.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import psycopg2
+from freezegun import freeze_time
 
 from odoo.exceptions import ValidationError
 from odoo.tools import mute_logger
@@ -34,19 +35,19 @@ class EDIBackendTestCase(EDIBackendCommonTestCase):
             }
             self.backend.create_record("test_csv_input", vals)
 
+    @freeze_time("2020-10-21 10:00:00")
     def test_create_record(self):
-        with self.mocked_today("2020-10-21 10:00:00"):
-            vals = {
-                "model": self.partner._name,
-                "res_id": self.partner.id,
-            }
-            record = self.backend.create_record("test_csv_input", vals)
-            expected = {
-                "type_id": self.exchange_type_in.id,
-                "edi_exchange_state": "new",
-                "exchange_filename": "EDI_EXC_TEST-test_csv_"
-                "input-2020-10-21-10-00-00.csv",
-            }
-            self.assertRecordValues(record, [expected])
-            self.assertEqual(record.record, self.partner)
-            self.assertEqual(record.edi_exchange_state, "new")
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+        }
+        record = self.backend.create_record("test_csv_input", vals)
+        expected = {
+            "type_id": self.exchange_type_in.id,
+            "edi_exchange_state": "new",
+            "exchange_filename": "EDI_EXC_TEST-test_csv_"
+            "input-2020-10-21-10-00-00.csv",
+        }
+        self.assertRecordValues(record, [expected])
+        self.assertEqual(record.record, self.partner)
+        self.assertEqual(record.edi_exchange_state, "new")
