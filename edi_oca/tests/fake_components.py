ce3cc3ce36d74c7546776cf6a2369651e742f66e
Simone Orsi <simahawk@gmail.com>
2020-11-29 11:06:21 +0100

    edi: test improve fake components, add input base

--- edi_oca/tests/fake_components.py
+++ edi_oca/tests/fake_components.py
@@ -5,44 +5,67 @@
 from odoo.addons.component.core import Component
 
 
-class FakeOutputGenerator(Component):
-    _name = "fake.output.generator"
-    _inherit = "edi.component.output.mixin"
-    _usage = "edi.output.demo_backend.test_csv_output"
+class FakeComponentMixin(Component):
 
-    def generate(self):
+    FAKED_COLLECTOR = []
+
+    def _fake_it(self):
         if self.env.context.get("test_break_it"):
             raise ValueError(self.env.context.get("test_break_it", "YOU BROKE IT!"))
-        return self.env.context.get(
-            "fake_output", "FAKE_OUTPUT: %d" % self.exchange_record.id
-        )
-
-
-class FakeOutputSender(Component):
-    _name = "fake.output.sender"
-    _inherit = "edi.component.send.mixin"
-    _usage = "edi.send.demo_backend.test_csv_output"
-
-    FAKE_SEND_COLLECTOR = []
-
-    def send(self):
-        if self.env.context.get("test_break_it"):
-            raise ValueError(self.env.context.get("test_break_it", "YOU BROKE IT!"))
-        self.FAKE_SEND_COLLECTOR.append(self.exchange_record._get_file_content())
-        return self.env.context.get(
-            "fake_output", "FAKE_OUTPUT: %d" % self.exchange_record.id
-        )
-
-
-class FakeOutputChecker(Component):
-    _name = "fake.output.checker"
-    _inherit = "edi.component.check.mixin"
-    _usage = "edi.check.demo_backend.test_csv_output"
-
-    def check(self):
-        if self.env.context.get("test_break_it"):
-            raise ValueError(self.env.context.get("test_break_it", "YOU BROKE IT!"))
-        update_values = self.env.context.get("test_output_check_update_values")
+        update_values = self.env.context.get("fake_update_values")
         if update_values:
             self.exchange_record.write(update_values)
-        return True
+        self.FAKED_COLLECTOR.append(self._call_key(self.exchange_record))
+        return self.env.context.get("fake_output", self._call_key(self.exchange_record))
+
+    @classmethod
+    def _call_key(cls, rec):
+        return "{}: {}".format(cls._name, rec.id)
+
+    @classmethod
+    def reset_faked(cls):
+        cls.FAKED_COLLECTOR = []
+
+    @classmethod
+    def check_called_for(cls, rec):
+        return cls._call_key(rec) in cls.FAKED_COLLECTOR
+
+    @classmethod
+    def check_not_called_for(cls, rec):
+        return cls._call_key(rec) not in cls.FAKED_COLLECTOR
+
+
+class FakeOutputGenerator(FakeComponentMixin):
+    _name = "fake.output.generator"
+    _inherit = "edi.component.output.mixin"
+    _usage = "edi.output.generate.demo_backend.test_csv_output"
+
+    def generate(self):
+        return self._fake_it()
+
+
+class FakeOutputSender(FakeComponentMixin):
+    _name = "fake.output.sender"
+    _inherit = "edi.component.send.mixin"
+    _usage = "edi.output.send.demo_backend.test_csv_output"
+
+    def send(self):
+        return self._fake_it()
+
+
+class FakeOutputChecker(FakeComponentMixin):
+    _name = "fake.output.checker"
+    _inherit = "edi.component.check.mixin"
+    _usage = "edi.output.check.demo_backend.test_csv_output"
+
+    def check(self):
+        return self._fake_it()
+
+
+class FakeInputProcess(FakeComponentMixin):
+    _name = "fake.input.process"
+    _inherit = "edi.component.input.mixin"
+    _usage = "edi.input.process.demo_backend.test_csv_input"
+
+    def process(self):
+        return self._fake_it()
