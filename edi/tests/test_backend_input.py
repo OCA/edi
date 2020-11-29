9880d933ae948dfdd5bdc80535a9927612b12fde
Enric Tobella <etobella@creublanca.es>
2020-11-29 12:43:24 +0100

    [IMP] edi: input processing, finishing tests

--- edi/tests/test_backend_input.py
+++ edi/tests/test_backend_input.py
@@ -4,7 +4,6 @@
 
 import base64
 
-import mock
 from freezegun import freeze_time
 
 from odoo import fields
@@ -12,8 +11,6 @@ from odoo.exceptions import UserError
 
 from .common import EDIBackendCommonComponentRegistryTestCase
 from .fake_components import FakeInputProcess
-
-# from odoo.tools import mute_logger
 
 
 class EDIBackendTestCase(EDIBackendCommonComponentRegistryTestCase):
@@ -49,28 +46,27 @@ class EDIBackendTestCase(EDIBackendCommonComponentRegistryTestCase):
         )
 
     def test_process_record_with_error(self):
-        vals = {
-            "model": self.partner._name,
-            "res_id": self.partner.id,
-            "edi_exchange_state": "input_received",
-            "exchange_file": base64.b64encode(b"1234"),
-        }
-        record = self.backend.create_record("test_csv_input", vals)
-        self.backend.exchange_process(record)
-        self.assertEqual(record.edi_exchange_state, "input_processed_error")
+        self.record.write({"edi_exchange_state": "input_received"})
+        self.record._set_file_content("TEST %d" % self.record.id)
+        self.record.with_context(
+            test_break_it="OOPS! Something went wrong :("
+        ).action_exchange_process()
+        self.assertTrue(FakeInputProcess.check_not_called_for(self.record))
+        self.assertRecordValues(
+            self.record,
+            [
+                {
+                    "edi_exchange_state": "input_processed_error",
+                    "exchange_error": "ValueError('OOPS! Something went wrong :(',)",
+                }
+            ],
+        )
 
     def test_process_no_file_record(self):
-        vals = {
-            "model": self.partner._name,
-            "res_id": self.partner.id,
-            "edi_exchange_state": "input_received",
-        }
-        record = self.backend.create_record("test_csv_input", vals)
-        with mock.patch.object(type(self.backend), "_exchange_process") as patch:
-            patch.return_value = "AAA"
-            with self.assertRaises(UserError):
-                self.backend.exchange_process(record)
-            patch.assert_not_called()
+        self.record.write({"edi_exchange_state": "input_received"})
+        self.record.exchange_file = False
+        with self.assertRaises(UserError):
+            self.record.action_exchange_process()
 
     def test_process_outbound_record(self):
         vals = {
@@ -78,8 +74,6 @@ class EDIBackendTestCase(EDIBackendCommonComponentRegistryTestCase):
             "res_id": self.partner.id,
         }
         record = self.backend.create_record("test_csv_output", vals)
-        with mock.patch.object(type(self.backend), "_exchange_process") as patch:
-            patch.return_value = "AAA"
-            with self.assertRaises(UserError):
-                self.backend.exchange_process(record)
-            patch.assert_not_called()
+        record._set_file_content("TEST %d" % record.id)
+        with self.assertRaises(UserError):
+            record.action_exchange_process()
