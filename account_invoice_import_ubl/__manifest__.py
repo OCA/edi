a0333f7b70ae6d6fc3acd41474241bd461a70ba7
Ronald Portier <ronald@therp.nl>
2020-11-30 10:54:51 +0100

    [IMP] account_invoice_import_ubl: black, isort, prettier

--- account_invoice_import_ubl/__manifest__.py
+++ account_invoice_import_ubl/__manifest__.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Invoice Import UBL',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'license': 'AGPL-3',
-    'summary': 'Import UBL XML supplier invoices/refunds',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
-    'depends': ['account_invoice_import', 'base_ubl'],
-    'data': ['wizard/account_invoice_import_view.xml'],
-    'demo': ['demo/demo_data.xml'],
-    'installable': True,
+    "name": "Account Invoice Import UBL",
+    "version": "12.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "summary": "Import UBL XML supplier invoices/refunds",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com",
+    "depends": ["account_invoice_import", "base_ubl"],
+    "data": ["wizard/account_invoice_import_view.xml"],
+    "demo": ["demo/demo_data.xml"],
+    "installable": True,
 }
