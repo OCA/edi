88d4d4804bd91816b001a5828c6aeb820577eb2b
Ronald Portier <ronald@therp.nl>
2020-11-30 11:17:44 +0100

    [MIG] account_invoice_import_ubl: migration to 13.0

--- account_invoice_import_ubl/wizard/account_invoice_import.py
+++ account_invoice_import_ubl/wizard/account_invoice_import.py
@@ -1,4 +1,4 @@
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import logging
