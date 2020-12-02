440fea45a1372f639675dad03b9eb1a670b4b0ec
Ronald Portier <ronald@therp.nl>
2020-12-02 17:30:27 +0100

    [FIX] account_invoice_import: fix headers

--- account_invoice_import/tests/test_invoice_import.py
+++ account_invoice_import/tests/test_invoice_import.py
@@ -1,4 +1,4 @@
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
