0a5765c600a99b64657df0c7d985c95ceb470931
Ronald Portier <ronald@therp.nl>
2020-12-02 17:30:27 +0100

    [FIX] account_invoice_import: fix headers

--- account_invoice_import/models/partner.py
+++ account_invoice_import/models/partner.py
@@ -1,5 +1,5 @@
-# © 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2015-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
