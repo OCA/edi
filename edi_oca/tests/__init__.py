4596751e04b1843e989284888baf551f57f379d8
Simone Orsi <simahawk@gmail.com>
2020-12-03 11:32:27 +0100

    edi.backend: fix handle reception of files

--- edi_oca/tests/__init__.py
+++ edi_oca/tests/__init__.py
@@ -3,6 +3,7 @@ from . import test_exchange_type
 from . import test_record
 from . import test_backend_base
 from . import test_backend_output
+from . import test_backend_input
 from . import test_backend_process
 from . import test_consumer_mixin
 from . import test_edi_backend_cron
