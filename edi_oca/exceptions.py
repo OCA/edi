95cf79c220823cd3ecb453e099a435fc1eaa7bb4
Simone Orsi <simahawk@gmail.com>
2020-12-07 13:55:21 +0100

    edi: improve validation handling

---
+++ edi_oca/exceptions.py
@@ -0,0 +1,8 @@
+# Copyright 2020 ACSONE
+# @author: Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+class EDIValidationError(Exception):
+    """Thrown when a document validation fails.
+    """
