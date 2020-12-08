cf7a1320ef50922516f58541311d601c5d2af745
Simone Orsi <simahawk@gmail.com>
2020-12-08 22:26:27 +0100

    edi.backend: rename generate to match others
    All the other main methods start with 'exchange_'.
    Make generate respect this convention.

--- edi_oca/tests/test_backend_validate.py
+++ edi_oca/tests/test_backend_validate.py
@@ -64,7 +64,7 @@ class EDIBackendTestCase(EDIBackendCommonComponentRegistryTestCase):
 
     def test_generate_validate_record(self):
         self.record_out.write({"edi_exchange_state": "new"})
-        self.backend.generate_output(self.record_out)
+        self.backend.exchange_generate(self.record_out)
         self.assertTrue(FakeOutputValidate.check_called_for(self.record_out))
         self.assertRecordValues(
             self.record_out, [{"edi_exchange_state": "output_pending"}]
@@ -73,7 +73,7 @@ class EDIBackendTestCase(EDIBackendCommonComponentRegistryTestCase):
     def test_generate_validate_record_error(self):
         self.record_out.write({"edi_exchange_state": "new"})
         exc = EDIValidationError("Data seems wrong!")
-        self.backend.with_context(test_break_validate=exc).generate_output(
+        self.backend.with_context(test_break_validate=exc).exchange_generate(
             self.record_out
         )
         self.assertTrue(FakeOutputValidate.check_called_for(self.record_out))
