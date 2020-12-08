d8a85c520b940fea6f59fdd44c20b92860099c8a
Enric Tobella <etobella@creublanca.es>
2020-12-08 13:14:43 +0100

    [IMP] edi: Allow to manually generate EDI Exchange records

--- edi_oca/tests/test_consumer_mixin.py
+++ edi_oca/tests/test_consumer_mixin.py
@@ -5,7 +5,10 @@
 import os
 import unittest
 
+from lxml import etree
 from odoo_test_helper import FakeModelLoader
+
+from odoo.tests.common import Form
 
 from .common import EDIBackendCommonTestCase
 
@@ -27,6 +30,29 @@ class TestConsumerMixinCase(EDIBackendCommonTestCase):
             {"name": "Test Consumer"}
         )
         cls.exchange_type_out.exchange_filename_pattern = "{record.id}"
+        cls.exchange_type_new = cls._create_exchange_type(
+            name="Test CSV output",
+            code="test_csv_new_output",
+            direction="output",
+            exchange_file_ext="csv",
+            backend_id=False,
+            exchange_filename_pattern="{record.ref}-{type.code}-{dt}",
+            model_ids=[(4, cls.env["ir.model"]._get_id(cls.consumer_record._name))],
+            enable_domain="[]",
+            enable_snippet="""result = not   record._has_exchange_record(
+            exchange_type.code)""",
+        )
+        cls.exchange_type_out.write(
+            {
+                "model_ids": [
+                    (4, cls.env["ir.model"]._get_id(cls.consumer_record._name),)
+                ],
+                "enable_domain": "[]",
+                "enable_snippet": """result = not   record._has_exchange_record(
+            exchange_type.code, exchange_type.backend_id)""",
+            }
+        )
+        cls.backend_02 = cls.backend.copy()
 
     @classmethod
     def tearDownClass(cls):
@@ -49,3 +75,68 @@ class TestConsumerMixinCase(EDIBackendCommonTestCase):
             exchange_record, self.env["edi.exchange.record"].search(action["domain"])
         )
         self.consumer_record._has_exchange_record(exchange_type, self.backend)
+
+    def test_expected_configuration(self):
+        self.assertTrue(self.consumer_record.has_expected_edi_configuration)
+        self.assertIn(
+            str(self.exchange_type_out.id),
+            self.consumer_record.expected_edi_configuration,
+        )
+        self.assertEqual(
+            self.consumer_record.expected_edi_configuration[
+                str(self.exchange_type_out.id)
+            ],
+            self.exchange_type_out.display_name,
+        )
+        action = self.consumer_record.edi_create_exchange_record(
+            self.exchange_type_out.id
+        )
+        self.assertEqual(action["res_model"], "edi.exchange.record")
+        self.consumer_record.refresh()
+        self.assertNotIn(
+            str(self.exchange_type_out.id),
+            self.consumer_record.expected_edi_configuration,
+        )
+        self.assertTrue(self.consumer_record.exchange_record_ids)
+        self.assertEqual(
+            self.consumer_record.exchange_record_ids.type_id, self.exchange_type_out
+        )
+
+    def test_multiple_backend(self):
+        self.assertIn(
+            str(self.exchange_type_new.id),
+            self.consumer_record.expected_edi_configuration,
+        )
+        action = self.consumer_record.edi_create_exchange_record(
+            self.exchange_type_new.id
+        )
+        self.assertNotEqual(action["res_model"], "edi.exchange.record")
+        self.assertEqual(action["res_model"], "edi.exchange.record.create")
+        wizard = (
+            self.env[action["res_model"]]
+            .with_context(**action["context"])
+            .create({"backend_id": self.backend_02.id})
+        )
+        wizard.create_edi()
+        self.consumer_record.refresh()
+        self.assertNotIn(
+            str(self.exchange_type_new.id),
+            self.consumer_record.expected_edi_configuration,
+        )
+        self.assertTrue(self.consumer_record.exchange_record_ids)
+        self.assertEqual(
+            self.consumer_record.exchange_record_ids.type_id, self.exchange_type_new
+        )
+
+    def test_form(self):
+        """Testing that the form has inherited the fields and inserted them
+        Unfortunately we are unable to test the buttons here
+        """
+        with Form(self.consumer_record) as f:
+            self.assertIn("has_expected_edi_configuration", f._values)
+            self.assertIn("expected_edi_configuration", f._values)
+            form = etree.fromstring(f._view["arch"])
+            self.assertTrue(
+                form.xpath("//field[@name='has_expected_edi_configuration']")
+            )
+            self.assertTrue(form.xpath("//field[@name='expected_edi_configuration']"))
