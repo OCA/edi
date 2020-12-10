558af1ee94a6195e8a024c3c903a94bc6339ae37
Enric Tobella <etobella@creublanca.es>
2020-12-10 16:06:38 +0100

    [ADD] webservice

---
+++ webservice/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "WebService",
+    "summary": """
+        Defines webservice abstract definition to be used generally""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "development_status": "Alpha",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["component", "server_environment"],
+    "data": ["security/ir.model.access.csv", "views/webservice_backend.xml"],
+    "demo": [],
+}
