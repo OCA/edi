c7a908ada1044bf8fb319e042cf0b5f976621723
Simone Orsi <simahawk@gmail.com>
2020-12-16 17:28:03 +0100

    edi: refactor component lookup
    The match is now done based on specific control keys: backend type, exchange type.
    This makes components more clean and their behavior more understandable.

--- edi_oca/components/base.py
+++ edi_oca/components/base.py
@@ -9,6 +9,9 @@ class EDIBackendComponentMixin(AbstractComponent):
 
     _name = "edi.component.mixin"
     _collection = "edi.backend"
+    _usage = None
+    _backend_type = None
+    _exchange_type = None
 
     @property
     def exchange_record(self):
@@ -21,3 +24,43 @@ class EDIBackendComponentMixin(AbstractComponent):
     @property
     def record(self):
         return self.work.exchange_record.record
+
+    @staticmethod
+    def _match_attrs():
+        """Attributes to be used for matching this component.
+
+        By default, match by backend and exchange type.
+
+        NOTE: the class attribute must have an underscore, the name here not.
+        """
+        return ("backend_type", "exchange_type")
+
+    @classmethod
+    def _component_match(cls, work, usage=None, model_name=None, **kw):
+        """Override to customize match.
+
+        Registry lookup filtered by usage and model_name when landing here.
+        Now, narrow match to `_match_attrs` attributes.
+        """
+        match_attrs = cls._match_attrs()
+        if not any([kw.get(k) for k in match_attrs]):
+            # No attr to check
+            return True
+
+        backend_type = kw.get("backend_type")
+        exchange_type = kw.get("exchange_type")
+
+        if cls._backend_type and cls._exchange_type:
+            # They must match both
+            return (
+                cls._backend_type == backend_type
+                and cls._exchange_type == exchange_type
+            )
+
+        if cls._backend_type not in (None, kw.get("backend_type")):
+            return False
+
+        if cls._exchange_type not in (None, kw.get("exchange_type")):
+            return False
+
+        return True
