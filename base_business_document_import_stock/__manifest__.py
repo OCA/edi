14cb40afd283cfd14d233bc71603120dc52c3a8c
Kevin.roche <kevin.roche@akretion.com>
2021-02-26 14:15:50 +0100

    [MIG] base_business_document_import_stock: Migration to 12.0

--- base_business_document_import_stock/__manifest__.py
+++ base_business_document_import_stock/__manifest__.py
@@ -3,17 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base Business Document Import Stock',
-    'version': '10.0.1.0.0',
-    'category': 'Hidden',
-    'license': 'AGPL-3',
-    'summary': 'Match incoterms upon import of business documents',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
-    'depends': [
-        'stock',
-        'base_business_document_import',
-        ],
-    'installable': True,
-    'auto_install': True,
+    "name": "Base Business Document Import Stock",
+    "version": "12.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "summary": "Match incoterms upon import of business documents",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com",
+    "depends": [
+        "stock",
+        "base_business_document_import",
+    ],
+    "installable": True,
+    "auto_install": True,
 }
--- base_business_document_import_stock/models/business_document_import.py
+++ base_business_document_import_stock/models/business_document_import.py
@@ -6,26 +6,33 @@ from odoo import models, api, _
 
 
 class BusinessDocumentImport(models.AbstractModel):
-    _inherit = 'business.document.import'
+    _inherit = "business.document.import"
 
     @api.model
     def _match_incoterm(self, incoterm_dict, chatter_msg):
-        sio = self.env['stock.incoterms']
+        sio = self.env["stock.incoterms"]
         if not incoterm_dict:
             return False
-        if incoterm_dict.get('recordset'):
-            return incoterm_dict['recordset']
-        if incoterm_dict.get('id'):
-            return sio.browse(incoterm_dict['id'])
-        if incoterm_dict.get('code'):
-            incoterms = sio.search([
-                '|',
-                ('name', '=ilike', incoterm_dict['code']),
-                ('code', '=ilike', incoterm_dict['code'])])
+        if incoterm_dict.get("recordset"):
+            return incoterm_dict["recordset"]
+        if incoterm_dict.get("id"):
+            return sio.browse(incoterm_dict["id"])
+        if incoterm_dict.get("code"):
+            incoterms = sio.search(
+                [
+                    "|",
+                    ("name", "=ilike", incoterm_dict["code"]),
+                    ("code", "=ilike", incoterm_dict["code"]),
+                ]
+            )
             if incoterms:
                 return incoterms[0]
             else:
-                chatter_msg.append(_(
-                    "Could not find any Incoterm in Odoo corresponding "
-                    "to '%s'") % incoterm_dict['code'])
+                chatter_msg.append(
+                    _(
+                        "Could not find any Incoterm in Odoo corresponding "
+                        "to '%s'"
+                    )
+                    % incoterm_dict["code"]
+                )
         return False
