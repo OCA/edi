5a2f1bed6a65bdd15ac7443621534e9defd61aad
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-05 19:08:32 +0100

    [MIG] account_invoice_import_facturx to v14.0

--- account_invoice_import_facturx/__manifest__.py
+++ account_invoice_import_facturx/__manifest__.py
@@ -1,14 +1,15 @@
-# Copyright 2015-2020 Akretion France (http://www.akretion.com/)
+# Copyright 2015-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Import Factur-X',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Invoicing Management',
     'license': 'AGPL-3',
     'summary': 'Import Factur-X/ZUGFeRD supplier invoices/refunds',
     'author': 'Akretion,Odoo Community Association (OCA)',
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/edi',
     'depends': ['account_invoice_import', 'base_facturx'],
     'external_dependencies': {'python': ['facturx']},
