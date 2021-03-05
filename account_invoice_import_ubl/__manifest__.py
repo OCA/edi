320cd438cca51e4a2d36703f431a6ebd8a6502de
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-06 00:35:07 +0100

    account_invoice_import_ubl: black, isort, etc.

--- account_invoice_import_ubl/__manifest__.py
+++ account_invoice_import_ubl/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Invoice Import UBL',
-    'version': '14.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'license': 'AGPL-3',
-    'summary': 'Import UBL XML supplier invoices/refunds',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/edi',
-    'depends': ['account_invoice_import', 'base_ubl'],
-    'data': ['wizard/account_invoice_import_view.xml'],
-    'demo': ['demo/demo_data.xml'],
-    'installable': True,
+    "name": "Account Invoice Import UBL",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "summary": "Import UBL XML supplier invoices/refunds",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/edi",
+    "depends": ["account_invoice_import", "base_ubl"],
+    "data": ["wizard/account_invoice_import_view.xml"],
+    "demo": ["demo/demo_data.xml"],
+    "installable": True,
 }
