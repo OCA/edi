e66fa7abe3546264e0efb528ab18ac5ee491354f
Alexis de Lattre <alexis.delattre@akretion.com>
2021-03-05 17:43:25 +0100

    [MIG] account_invoice_import_invoice2data to v14.0

--- account_invoice_import_invoice2data/__manifest__.py
+++ account_invoice_import_invoice2data/__manifest__.py
@@ -1,14 +1,15 @@
-# Copyright 2015-2020 Akretion France (http://www.akretion.com/)
+# Copyright 2015-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Import Invoice2data',
-    'version': '12.0.1.0.0',
-    'category': 'Invoicing Management',
+    'version': '14.0.1.0.0',
+    'category': 'Accounting/Accounting',
     'license': 'AGPL-3',
     'summary': 'Import supplier invoices using the invoice2data lib',
     'author': 'Akretion,Odoo Community Association (OCA)',
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/edi',
     'depends': ['account_invoice_import'],
     'external_dependencies': {'python': ['invoice2data']},
--- account_invoice_import_invoice2data/tests/test_invoice_import.py
+++ account_invoice_import_invoice2data/tests/test_invoice_import.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2020 Akretion France (www.akretion.com)
+# Copyright 2015-2021 Akretion France (www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -11,20 +11,12 @@ import base64
 class TestInvoiceImport(TransactionCase):
 
     def setUp(self):
-        super(TestInvoiceImport, self).setUp()
+        super().setUp()
         frtax = self.env['account.tax'].create({
             'name': 'French VAT purchase 20.0%',
             'description': 'FR-VAT-buy-20.0',
             'amount': 20,
             'amount_type': 'percent',
-            'account_id': self.env['account.account'].search([
-                ('user_type_id', '=',
-                 self.env.ref('account.data_account_type_expenses').id)
-            ], limit=1).id,
-            'refund_account_id': self.env['account.account'].search([
-                ('user_type_id', '=',
-                 self.env.ref('account.data_account_type_expenses').id)
-            ], limit=1).id,
             'type_tax_use': 'purchase',
             })
         # Set this tax on Internet access product
@@ -37,73 +29,68 @@ class TestInvoiceImport(TransactionCase):
         f = file_open(
             'account_invoice_import_invoice2data/tests/pdf/' + filename, 'rb')
         pdf_file = f.read()
+        pdf_file_b64 = base64.b64encode(pdf_file)
         wiz = self.env['account.invoice.import'].create({
-            'invoice_file': base64.b64encode(pdf_file),
+            'invoice_file': pdf_file_b64,
             'invoice_filename': filename,
         })
         f.close()
         wiz.import_invoice()
         # Check result of invoice creation
-        invoices = self.env['account.invoice'].search([
+        invoices = self.env['account.move'].search([
             ('state', '=', 'draft'),
-            ('type', '=', 'in_invoice'),
-            ('reference', '=', '562044387')
+            ('move_type', '=', 'in_invoice'),
+            ('ref', '=', '562044387')
             ])
-        self.assertEquals(len(invoices), 1)
+        self.assertEqual(len(invoices), 1)
         inv = invoices[0]
-        self.assertEquals(inv.type, 'in_invoice')
-        self.assertEquals(
-            fields.Date.to_string(inv.date_invoice), '2015-07-02')
-        self.assertEquals(
+        self.assertEqual(inv.move_type, 'in_invoice')
+        self.assertEqual(
+            fields.Date.to_string(inv.invoice_date), '2015-07-02')
+        self.assertEqual(
             inv.partner_id,
             self.env.ref('account_invoice_import_invoice2data.free'))
-        self.assertEquals(inv.journal_id.type, 'purchase')
-        self.assertEquals(
+        self.assertEqual(inv.journal_id.type, 'purchase')
+        self.assertEqual(
             float_compare(inv.amount_total, 29.99, precision_digits=2), 0)
-        self.assertEquals(
+        self.assertEqual(
             float_compare(inv.amount_untaxed, 24.99, precision_digits=2), 0)
-        self.assertEquals(
+        self.assertEqual(
             len(inv.invoice_line_ids), 1)
         iline = inv.invoice_line_ids[0]
-        self.assertEquals(iline.name, 'Fiber optic access at the main office')
-        self.assertEquals(
+        self.assertEqual(iline.name, 'Fiber optic access at the main office')
+        self.assertEqual(
             iline.product_id,
             self.env.ref(
                 'account_invoice_import_invoice2data.internet_access'))
-        self.assertEquals(
+        self.assertEqual(
             float_compare(iline.quantity, 1.0, precision_digits=0), 0)
-        self.assertEquals(
+        self.assertEqual(
             float_compare(iline.price_unit, 24.99, precision_digits=2), 0)
 
         # Prepare data for next test i.e. invoice update
         # (we re-use the invoice created by the first import !)
         inv.write({
-            'date_invoice': False,
-            'reference': False,
+            'invoice_date': False,
+            'ref': False,
             })
 
         # New import with update of an existing draft invoice
-        f = file_open(
-            'account_invoice_import_invoice2data/tests/pdf/'
-            'invoice_free_fiber_201507.pdf',
-            'rb')
-        pdf_file = f.read()
         wiz2 = self.env['account.invoice.import'].create({
-            'invoice_file': base64.b64encode(pdf_file),
+            'invoice_file': pdf_file_b64,
             'invoice_filename': 'invoice_free_fiber_201507.pdf',
             })
-        f.close()
         action = wiz2.import_invoice()
-        self.assertEquals(
+        self.assertEqual(
             action['res_model'], 'account.invoice.import')
         # Choose to update the existing invoice
         wiz2.update_invoice()
-        invoices = self.env['account.invoice'].search([
+        invoices = self.env['account.move'].search([
             ('state', '=', 'draft'),
-            ('type', '=', 'in_invoice'),
-            ('reference', '=', '562044387')
+            ('move_type', '=', 'in_invoice'),
+            ('ref', '=', '562044387')
             ])
-        self.assertEquals(len(invoices), 1)
+        self.assertEqual(len(invoices), 1)
         inv = invoices[0]
-        self.assertEquals(
-            fields.Date.to_string(inv.date_invoice), '2015-07-02')
+        self.assertEqual(
+            fields.Date.to_string(inv.invoice_date), '2015-07-02')
