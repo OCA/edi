620137bb6c3939cf396d0919f265488c0231d716
Enric Tobella <etobella@creublanca.es>
2021-03-10 13:56:20 +0100

    [CHG] edi: Use more permissive licence: AGPL-> LGPL

--- edi_account_oca/__manifest__.py
+++ edi_account_oca/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2020 Creu Blanca
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Edi Account",
     "summary": """
         Define EDI Configuration for Account Moves""",
     "version": "13.0.1.0.1",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
     "depends": ["account", "edi", "component_event"],
