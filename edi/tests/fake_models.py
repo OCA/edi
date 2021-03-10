b14a8b39d86e95b43a5095a23b0c61e208793fbb
Enric Tobella <etobella@creublanca.es>
2021-03-10 13:56:20 +0100

    [CHG] edi: Use more permissive licence: AGPL-> LGPL

--- edi/tests/fake_models.py
+++ edi/tests/fake_models.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Creu Blanca
 # @author: Enric Tobella
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
 
