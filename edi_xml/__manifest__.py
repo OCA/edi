fdf7a241b3da2aee80468364e875568f45cdfc43
Enric Tobella <etobella@creublanca.es>
2021-03-10 13:56:20 +0100

    [CHG] edi: Use more permissive licence: AGPL-> LGPL

--- edi_xml/__manifest__.py
+++ edi_xml/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2020 ACSONE
 # @author: Simone Orsi <simahawk@gmail.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "EDI XML",
@@ -9,7 +9,7 @@
     """,
     "version": "13.0.1.2.0",
     "development_status": "Alpha",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "ACSONE,Odoo Community Association (OCA)",
     "depends": ["edi", "component"],
     "external_dependencies": {"python": ["xmlschema"]},
