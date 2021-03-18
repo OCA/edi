7eba9d40e564dc60b5de8a1ed24502e01abd670b
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-03-18 10:48:56 -0300

    [ADD] account_invoice_facturx_buyer_ref

---
+++ account_invoice_facturx_buyer_ref/models/account_invoice.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Camptocamp
+# @author: Iván Todorovich <ivan.todorovich@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.model
+    def _cii_trade_agreement_buyer_ref(self, partner):
+        return (
+            partner.facturx_ref
+            or partner.commercial_partner_id.facturx_ref
+            or super()._cii_trade_agreement_buyer_ref(partner)
+        )
