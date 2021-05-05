8faf61aa1376f494b3d8e9f3f0c64cf685b4d014
fshah <foram.shah@initos.com>
2021-05-05 15:07:19 +0530

    [MIG] Migrate module edi_storage_oca to v14.

--- edi_storage_oca/__manifest__.py
+++ edi_storage_oca/__manifest__.py
@@ -7,12 +7,12 @@
     "summary": """
     Base module to allow exchanging files via storage backend (eg: SFTP).
     """,
-    "version": "13.0.1.5.0",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/edi",
     "author": "ACSONE,Odoo Community Association (OCA)",
-    "depends": ["edi", "storage_backend", "component"],
+    "depends": ["edi_oca", "storage_backend", "component"],
     "data": ["security/ir_model_access.xml", "views/edi_backend_views.xml"],
     "demo": ["demo/edi_backend_demo.xml"],
 }
