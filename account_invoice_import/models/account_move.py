7a96ac743830065d000e639cd304244727925f1f
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-26 01:11:55 +0200

    [MIG] account_invoice_import to v14
    Add support for partner creation

--- account_invoice_import/models/account_move.py
+++ account_invoice_import/models/account_move.py
@@ -1,26 +1,19 @@
-# Copyright 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2015-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, models
+from odoo.tools.misc import format_amount
 
 
-class AccountInvoice(models.Model):
+class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def name_get(self):
+    def _get_move_display_name(self, show_ref=False):
         """Add amount_untaxed in name_get of invoices"""
-        res = super().name_get()
+        name = super()._get_move_display_name(show_ref=show_ref)
         if self.env.context.get("invoice_show_amount"):
-            new_res = []
-            for (inv_id, name) in res:
-                inv = self.browse(inv_id)
-                # I didn't find a python method to easily display
-                # a float + currency symbol (before or after)
-                # depending on lang of context and currency
-                name += _(" Amount w/o tax: %s %s") % (
-                    inv.amount_untaxed,
-                    inv.currency_id.name,
-                )
-                new_res.append((inv_id, name))
-            return new_res
-        return res
+            name += _(" Amount w/o tax: %s") % format_amount(
+                self.env, self.amount_untaxed, self.currency_id
+            )
+        return name
