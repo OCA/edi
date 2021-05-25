9919c041061f1546f95c1675272be686dbb6bed0
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-26 01:11:55 +0200

    [MIG] account_invoice_import to v14
    Add support for partner creation

--- account_invoice_import/models/__init__.py
+++ account_invoice_import/models/__init__.py
@@ -1,5 +1,4 @@
-from . import partner
-from . import company
-from . import res_config_settings
+from . import res_partner
+from . import res_company
 from . import account_invoice_import_config
 from . import account_move
