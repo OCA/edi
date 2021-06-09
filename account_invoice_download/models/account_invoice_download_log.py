837053eec7e3d3da93daa034b36bcdcb6444e7b3
Alexis de Lattre <alexis.delattre@akretion.com>
2021-06-10 00:51:45 +0200

    account_invoice_download: Migrate from v12 to v14
    Add pivot and graph views on download logs
    Code cleanup

--- account_invoice_download/models/account_invoice_download_log.py
+++ account_invoice_download/models/account_invoice_download_log.py
@@ -1,8 +1,8 @@
-# Copyright 2017-2018 Akretion France
+# Copyright 2017-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountInvoiceDownloadLog(models.Model):
@@ -15,14 +15,11 @@ class AccountInvoiceDownloadLog(models.Model):
         'account.invoice.download.config', string='Download Config',
         readonly=True)
     import_config_id = fields.Many2one(
-        related='download_config_id.import_config_id',
-        readonly=True, store=True)
+        related='download_config_id.import_config_id', store=True)
     partner_id = fields.Many2one(
-        related='download_config_id.import_config_id.partner_id',
-        readonly=True, store=True)
+        related='download_config_id.import_config_id.partner_id', store=True)
     company_id = fields.Many2one(
-        'res.company', related='download_config_id.company_id',
-        store=True, readonly=True)
+        'res.company', related='download_config_id.company_id', store=True)
     message = fields.Text(string='Message', readonly=True)
     result = fields.Selection([
         ('success', 'Success'),
---
+++ account_invoice_download/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module is the technical basis to automate the download of supplier invoices with Odoo. It must be used in combination with additionnal modules that add download backends. The technical name of these additionnal modules usually start with **account\_invoice\_download\_**.
