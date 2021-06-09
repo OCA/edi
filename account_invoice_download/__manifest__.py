4511c7b185c828ed43549b5b2f7405088bcc4458
Alexis de Lattre <alexis.delattre@akretion.com>
2021-06-10 00:58:20 +0200

    account_invoice_download: black, isort, etc.

--- account_invoice_download/__manifest__.py
+++ account_invoice_download/__manifest__.py
@@ -3,25 +3,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Invoice Download',
-    'version': '14.0.1.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'summary': 'Auto-download supplier invoices and import them',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/edi',
-    'depends': [
-        'account_invoice_import',
-        ],
-    'data': [
-        'security/rule.xml',
-        'security/ir.model.access.csv',
-        'views/account_invoice_download_config.xml',
-        'views/account_invoice_download_log.xml',
-        'views/account_invoice_import_config.xml',
-        'wizard/account_invoice_download_credentials_view.xml',
-        'data/cron.xml',
+    "name": "Account Invoice Download",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Auto-download supplier invoices and import them",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/edi",
+    "depends": [
+        "account_invoice_import",
     ],
-    'installable': True,
+    "data": [
+        "security/rule.xml",
+        "security/ir.model.access.csv",
+        "views/account_invoice_download_config.xml",
+        "views/account_invoice_download_log.xml",
+        "views/account_invoice_import_config.xml",
+        "wizard/account_invoice_download_credentials_view.xml",
+        "data/cron.xml",
+    ],
+    "installable": True,
 }
