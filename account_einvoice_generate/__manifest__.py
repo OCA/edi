fbe983c42622c8696a547b89e19628e8a57700bf
dsolanki <dhara.solanki@initos.com>
2021-06-10 12:22:49 +0530

    [MIG] MIgrate module account_einvoice_generate to v14.

--- account_einvoice_generate/__manifest__.py
+++ account_einvoice_generate/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account e-invoice Generate",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "Technical module to generate PDF invoices with " "embedded XML file",
