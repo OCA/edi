2f30a33035581123cf54c8e73fad0d1fc096763e
Lois Rilo <lois.rilo@forgeflow.com>
2021-07-01 12:57:49 +0200

    [FIX] edi_storage_oca: support regex pattern searching when fetching filenames from storage.
    Also extend help in `exchange_filename_pattern` field to clarify usage.

--- edi_storage_oca/models/edi_backend.py
+++ edi_storage_oca/models/edi_backend.py
@@ -148,12 +148,13 @@ class EDIBackend(models.Model):
             # If there is not patter, return everything
             return self.storage_id.list_files(self.input_dir_pending)
 
-        # TODO: validate pattern usage.
         bits = [exchange_type.exchange_filename_pattern]
         if exchange_type.exchange_file_ext:
-            bits.append("*." + exchange_type.exchange_file_ext)
+            bits.append(r"\." + exchange_type.exchange_file_ext)
         pattern = "".join(bits)
-        return self.storage_id.find_files(pattern, self.input_dir_pending)
+        full_paths = self.storage_id.find_files(pattern, self.input_dir_pending)
+        pending_path_len = len(self.input_dir_pending)
+        return [p[pending_path_len:].strip("/") for p in full_paths]
 
     def _storage_new_exchange_record_vals(self, file_name):
         return {"exchange_filename": file_name, "edi_exchange_state": "input_pending"}
