ef132d1939b5b11741bb2fd508fe6c3fdb7ece65
Lois Rilo <lois.rilo@forgeflow.com>
2021-07-01 09:29:10 +0200

    [IMP] edi_storage_oca: add specific channel and job function definitions

--- edi_storage_oca/__manifest__.py
+++ edi_storage_oca/__manifest__.py
@@ -15,6 +15,8 @@
     "depends": ["edi_oca", "storage_backend", "component"],
     "data": [
         "data/cron.xml",
+        "data/job_channel_data.xml",
+        "data/queue_job_function_data.xml",
         "security/ir_model_access.xml",
         "views/edi_backend_views.xml",
     ],
