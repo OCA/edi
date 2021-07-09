14f6cba7f490535f6c6f7e560b0c56dc79d372af
Simone Orsi <simahawk@gmail.com>
2021-07-09 09:58:33 +0200

    edi_gs1_stock: DRAFT input

---
+++ edi_gs1_stock/components/input_mixin.py
@@ -0,0 +1,24 @@
+# Copyright 2020 ACSONE
+# @author: Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, exceptions
+from odoo.tools import DotDict
+
+from odoo.addons.component.core import AbstractComponent
+
+
+class GS1InputShipmentMessageMixin(AbstractComponent):
+    """Common gs1 input shipment mixin.
+    """
+
+    _name = "edi.gs1.input.shipment.mixin"
+    _inherit = [
+        "edi.gs1.input.mixin",
+    ]
+
+    def _process_data(self, data):
+        import pdb
+
+        pdb.set_trace()
+        pass
