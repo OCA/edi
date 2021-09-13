635a3a1e8529e65846fcd89f4d8620ccb483a19c
Simone Orsi <simahawk@gmail.com>
2021-09-13 16:08:22 +0200

    Add endpoint module

---
+++ endpoint/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Camptcamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Endpoint",
+    "summary": """Provide custom endpoint machinery.""",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "development_status": "Alpha",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "maintainers": ["simahawk"],
+    "website": "https://github.com/OCA/edi",
+    "data": [
+        "security/ir.model.access.csv",
+        "demo/endpoint_demo.xml",
+        "views/endpoint_view.xml",
+    ],
+}
