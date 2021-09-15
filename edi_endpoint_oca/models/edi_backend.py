80fc8007036154d76c6b97756c78216a258ff35f
Simone Orsi <simahawk@gmail.com>
2021-09-15 16:43:36 +0200

    Add edi_endpoint_oca

---
+++ edi_endpoint_oca/models/edi_backend.py
@@ -0,0 +1,29 @@
+# Copyright 2021 Camptcamp SA
+# @author: Simone Orsi <simone.orsi@camptocamp.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class EDIBackend(models.Model):
+
+    _inherit = "edi.backend"
+
+    endpoint_ids = fields.One2many(
+        string="Endpoints",
+        comodel_name="edi.endpoint",
+        inverse_name="backend_id",
+    )
+
+    def action_manage_endpoints(self):
+        action = self.env.ref("edi_endpoint_oca.edi_endpoint_act_window").read()[0]
+        action["domain"] = [
+            "|",
+            ("backend_id", "=", False),
+            ("backend_id", "=", self.id),
+        ]
+        action["context"] = {
+            "default_backend_id": self.id,
+            "default_backend_type_id": self.backend_type_id.id,
+        }
+        return action
