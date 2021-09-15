9462cac72994468f10e38c7eb3ed9f8d32142983
Simone Orsi <simahawk@gmail.com>
2021-09-15 16:43:36 +0200

    Add edi_endpoint_oca

---
+++ edi_endpoint_oca/models/__init__.py
@@ -0,0 +1,3 @@
+from . import edi_backend
+from . import edi_backend_type
+from . import edi_endpoint
---
+++ edi_endpoint_oca/models/edi_backend_type.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Camptcamp SA
+# @author: Simone Orsi <simone.orsi@camptocamp.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+# TODO add view
+class EDIBackendType(models.Model):
+
+    _inherit = "edi.backend.type"
+
+    endpoint_ids = fields.One2many(
+        string="Endpoints",
+        comodel_name="edi.endpoint",
+        inverse_name="backend_type_id",
+    )
