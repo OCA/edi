8fe93c91872f912d0af3e242631fa371dc50009e
Alexis de Lattre <alexis.delattre@akretion.com>
2021-10-04 09:31:19 +0200

    purchase_order_ubl_py3o: black, isort, etc.

--- purchase_order_ubl_py3o/__manifest__.py
+++ purchase_order_ubl_py3o/__manifest__.py
@@ -3,13 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Py3o UBL Purchase Order',
-    'version': '14.0.1.0.0',
-    'category': 'Purchases',
-    'license': 'AGPL-3',
-    'summary': 'Generate UBL purchase orders with Py3o reporting engine',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/edi',
-    'depends': ['purchase_order_ubl', 'report_py3o_fusion_server'],
-    'installable': True,
+    "name": "Py3o UBL Purchase Order",
+    "version": "14.0.1.0.0",
+    "category": "Purchases",
+    "license": "AGPL-3",
+    "summary": "Generate UBL purchase orders with Py3o reporting engine",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["purchase_order_ubl", "report_py3o_fusion_server"],
+    "installable": True,
 }
