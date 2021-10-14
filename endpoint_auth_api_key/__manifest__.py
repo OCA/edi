e7b95ff14fbf461358685aa6d8c8fe891af265ab
Simone Orsi <simahawk@gmail.com>
2021-10-14 13:54:03 +0200

    fixup! Add endpoint_auth_api_key

--- endpoint_auth_api_key/__manifest__.py
+++ endpoint_auth_api_key/__manifest__.py
@@ -7,10 +7,10 @@
     "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "development_status": "Alpha",
-    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["simahawk"],
     "website": "https://github.com/OCA/edi",
-    "depends": ["auth_api_key_group"],
+    "depends": ["endpoint", "auth_api_key_group"],
     "demo": ["demo/api_key_demo.xml", "demo/endpoint_demo.xml"],
     "data": ["views/endpoint_view.xml"],
 }
