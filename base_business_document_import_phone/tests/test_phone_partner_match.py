49411d65ea243c58bd55eae6e6294204ba3503c7
Alexis de Lattre <alexis.delattre@akretion.com>
2021-11-19 10:44:57 +0100

    [MIG] base_business_document_import_phone to v14 (from v12)

--- base_business_document_import_phone/tests/test_phone_partner_match.py
+++ base_business_document_import_phone/tests/test_phone_partner_match.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2019 Akretion France (http://www.akretion.com/)
+# Copyright 2016-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -11,21 +11,21 @@ class TestPhonePartnerMatch(TransactionCase):
         rpo = self.env['res.partner']
         bdoo = self.env['business.document.import']
         partner = rpo.create({
-            'name': u'Alexis de Lattre',
+            'name': 'Alexis de Lattre',
             'country_id': self.env.ref('base.fr').id,
-            'supplier': True,
             'phone': '+33141981242',
             'mobile': '+33699887766',
+            'supplier_rank': 10,
             })
         partner_dict = {
             'country_code': 'FR',
             'phone': '01.41.98.12.42',
             }
-        res = bdoo._match_partner(partner_dict, [], partner_type='supplier')
-        self.assertEquals(res, partner)
+        res = bdoo._match_partner(partner_dict, [])
+        self.assertEqual(res, partner)
         partner_dict = {
             'country_code': 'FR',
             'phone': '(0)6-99-88-77-66',
             }
-        res = bdoo._match_partner(partner_dict, [], partner_type='supplier')
-        self.assertEquals(res, partner)
+        res = bdoo._match_partner(partner_dict, [])
+        self.assertEqual(res, partner)
