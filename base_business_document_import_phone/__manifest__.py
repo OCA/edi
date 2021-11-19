58d147f7941a60fccc57fa2686bf595629f5d729
Alexis de Lattre <alexis.delattre@akretion.com>
2021-11-19 10:46:39 +0100

    base_business_document_import_phone: black, isort, etc.

--- base_business_document_import_phone/__manifest__.py
+++ base_business_document_import_phone/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base Business Document Import Phone',
-    'version': '14.0.1.0.0',
-    'category': 'Hidden',
-    'license': 'AGPL-3',
-    'summary': 'Use phone numbers to match partners upon import of '
-               'business documents',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/edi',
-    'depends': [
-        'phone_validation',
-        'base_business_document_import',
-        ],
-    'external_dependencies': {'python': ['phonenumbers']},
-    'installable': True,
-    'auto_install': True,
+    "name": "Base Business Document Import Phone",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "summary": "Use phone numbers to match partners upon import of "
+    "business documents",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/edi",
+    "depends": [
+        "phone_validation",
+        "base_business_document_import",
+    ],
+    "external_dependencies": {"python": ["phonenumbers"]},
+    "installable": True,
+    "auto_install": True,
 }
