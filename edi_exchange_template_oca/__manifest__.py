98808d59c77e8bcced55b0a99dd660e3dc4d8efe
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-07 11:18:53 +0100

    edi_exchange_template_oca 14.0.1.0.1

--- edi_exchange_template_oca/__manifest__.py
+++ edi_exchange_template_oca/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "EDI Exchange Template",
     "summary": """Allows definition of exchanges via templates.""",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "development_status": "Beta",
     "license": "LGPL-3",
     "author": "ACSONE,Odoo Community Association (OCA)",
