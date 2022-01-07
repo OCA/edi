b28846426ff37e06af30bbf13a8801b51c755589
Enric Tobella <etobella@creublanca.es>
2022-01-07 08:30:01 +0100

    [UPD] edi_exchange_template_oca: Mark as mature

--- edi_exchange_template_oca/__manifest__.py
+++ edi_exchange_template_oca/__manifest__.py
@@ -6,7 +6,6 @@
     "name": "EDI Exchange Template",
     "summary": """Allows definition of exchanges via templates.""",
     "version": "14.0.1.0.0",
-    "development_status": "Beta",
     "license": "LGPL-3",
     "author": "ACSONE,Odoo Community Association (OCA)",
     "maintainers": ["simahawk"],
