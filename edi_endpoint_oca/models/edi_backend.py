884de0659e7bcbe746aa2bc455ce7901f2f5c8bc
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-01-31 11:12:58 +0100

    [FIX] Fix access right issues when using actions

---
+++ edi_endpoint_oca/models/edi_backend.py
@@ -0,0 +1,37 @@
+# Copyright 2021 Camptcamp SA
+# @author: Simone Orsi <simone.orsi@camptocamp.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class EDIBackend(models.Model):
+
+    _inherit = "edi.backend"
+
+    endpoint_ids = fields.One2many(
+        string="Endpoints",
+        comodel_name="edi.endpoint",
+        inverse_name="backend_id",
+    )
+    endpoints_count = fields.Integer(compute="_compute_endpoints_count")
+
+    @api.depends("endpoint_ids")
+    def _compute_endpoints_count(self):
+        for record in self:
+            record.endpoints_count = len(record.endpoint_ids)
+
+    def action_manage_endpoints(self):
+        xmlid = "edi_endpoint_oca.edi_endpoint_act_window"
+        action = self.env["ir.actions.act_window"]._for_xml_id(xmlid)
+        action["domain"] = [
+            ("backend_type_id", "=", self.backend_type_id),
+            "|",
+            ("backend_id", "=", False),
+            ("backend_id", "=", self.id),
+        ]
+        action["context"] = {
+            "default_backend_id": self.id,
+            "default_backend_type_id": self.backend_type_id.id,
+        }
+        return action
