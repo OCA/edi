c6db6bbfdd44628f833335b3653d14b97bf49a7a
Lois Rilo <lois.rilo@forgeflow.com>
2022-02-14 16:41:16 +0100

    [ADD] edi_purchase_oca

---
+++ edi_purchase_oca/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "EDI Purchase",
+    "summary": """
+        Define EDI Configuration for Purchase Orders""",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["purchase", "edi", "component_event"],
+    "data": ["views/purchase_order_views.xml", "views/edi_exchange_record_views.xml"],
+    "demo": [],
+}
