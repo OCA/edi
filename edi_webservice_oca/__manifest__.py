ef7d46c5bb72c53484086a08aa3eba2db2464628
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-03-03 09:14:56 +0100

    [IMP] edi_webservice: Promote to Beta
    Being used in some places like OCA/l10n-spain for a while

--- edi_webservice_oca/__manifest__.py
+++ edi_webservice_oca/__manifest__.py
@@ -7,7 +7,7 @@
         Defines webservice integration from EDI Exchange records""",
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
     "depends": ["edi_oca", "webservice"],
