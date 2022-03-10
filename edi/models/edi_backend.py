e37a60b117ac5591599829e9514a5b437b6742bd
Simone Orsi <simahawk@gmail.com>
2022-03-10 15:02:32 +0100

    edi: backend action to view exchanges

--- edi/models/edi_backend.py
+++ edi/models/edi_backend.py
@@ -606,3 +606,14 @@ class EDIBackend(models.Model):
             ("type_id", "=", exchange_type.id),
         ] + extra_domain or []
         return self.env["edi.exchange.record"].search(domain, count=count_only)
+
+    def action_view_exchanges(self):
+        action = self.env["ir.actions.act_window"].for_xml_id(
+            "edi", "act_open_edi_exchange_record_view"
+        )
+        action["context"] = {
+            "search_default_backend_id": self.id,
+            "default_backend_id": self.id,
+            "default_backend_type_id": self.backend_type_id.id,
+        }
+        return action
