3c7c1c403ef84c5d3cadb472982cc15ab2c3190f
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-04-06 12:13:58 +0200

    [MIG] edi_purchase_oca: Migration to 14.0

--- edi_purchase_oca/__manifest__.py
+++ edi_purchase_oca/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "EDI Purchase",
     "summary": """
         Define EDI Configuration for Purchase Orders""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
-    "depends": ["purchase", "edi", "component_event"],
+    "depends": ["purchase", "edi_oca", "component_event"],
     "data": ["views/purchase_order_views.xml", "views/edi_exchange_record_views.xml"],
     "demo": [],
 }
