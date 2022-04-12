7d229ce961094b39d375a48c291b9b589b1c798b
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-04-12 16:45:22 +0530

    [IMP] edi_storage_oca: Update development status

--- edi_storage_oca/__manifest__.py
+++ edi_storage_oca/__manifest__.py
@@ -8,7 +8,7 @@
     Base module to allow exchanging files via storage backend (eg: SFTP).
     """,
     "version": "14.0.1.5.1",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/edi",
     "author": "ACSONE,Odoo Community Association (OCA)",
