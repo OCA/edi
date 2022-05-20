e73e1432e286e09a076dd4cef02e11ad38ebb83c
Simone Orsi <simahawk@gmail.com>
2022-05-20 13:00:36 +0200

    sale_order_import: update autorship

--- sale_order_import/wizard/sale_order_import.py
+++ sale_order_import/wizard/sale_order_import.py
@@ -1,4 +1,7 @@
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2017 Akretion
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2022 Camptocamp
+# @author: Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import logging
