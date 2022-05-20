5487aa00a0e3c6ca2c1712f5744197f9d347123a
Simone Orsi <simahawk@gmail.com>
2022-05-20 09:54:20 +0200

    sale_order_import: refactor tests setup

--- sale_order_import/tests/test_order_import.py
+++ sale_order_import/tests/test_order_import.py
@@ -1,13 +1,18 @@
 # Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2022 Camptocamp SA
+# @author: Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestOrderImport(TransactionCase):
-    def test_order_import(self):
-        soio = self.env["sale.order.import"]
-        parsed_order = {
+class TestOrderImport(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.wiz_model = cls.env["sale.order.import"]
+        cls.parsed_order = {
             "partner": {"email": "deco.addict82@example.com"},
             "date": "2018-08-14",
             "order_ref": "TEST1242",
@@ -22,11 +27,13 @@ class TestOrderImport(TransactionCase):
             "chatter_msg": [],
             "doc_type": "rfq",
         }
-        order = soio.create_order(parsed_order, "pricelist")
-        self.assertEqual(order.client_order_ref, parsed_order["order_ref"])
+
+    def test_order_import(self):
+        order = self.wiz_model.create_order(self.parsed_order, "pricelist")
+        self.assertEqual(order.client_order_ref, self.parsed_order["order_ref"])
         self.assertEqual(
             order.order_line[0].product_id.default_code,
-            parsed_order["lines"][0]["product"]["code"],
+            self.parsed_order["lines"][0]["product"]["code"],
         )
         self.assertEqual(int(order.order_line[0].product_uom_qty), 2)
         # Now update the order
@@ -51,6 +58,6 @@ class TestOrderImport(TransactionCase):
             "chatter_msg": [],
             "doc_type": "rfq",
         }
-        soio.update_order_lines(parsed_order_up, order, "pricelist")
+        self.wiz_model.update_order_lines(parsed_order_up, order, "pricelist")
         self.assertEqual(len(order.order_line), 2)
         self.assertEqual(int(order.order_line[0].product_uom_qty), 3)
