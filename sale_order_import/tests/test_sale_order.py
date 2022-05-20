be8d68f864314c8c84f5b6c6034e8c62cf903906
Simone Orsi <simahawk@gmail.com>
2022-05-20 09:54:20 +0200

    sale_order_import: refactor tests setup

--- sale_order_import/tests/test_sale_order.py
+++ sale_order_import/tests/test_sale_order.py
@@ -1,10 +1,18 @@
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2022 Camptocamp SA
+# @author: Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestSaleOrder(TransactionCase):
+class TestOrderImport(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+
     def test_name_get(self):
         sale_order = self.env.ref("sale.sale_order_1")
         name = sale_order.name + _(" Amount w/o tax: %s %s)") % (
