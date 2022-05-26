bae8cbac17bad02cc63ea7ee2945aa32d650b784
Simone Orsi <simahawk@gmail.com>
2022-05-26 11:46:04 +0200

    sale_order_import: refactor parse_order, de-dup code

--- sale_order_import/__manifest__.py
+++ sale_order_import/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Import",
-    "version": "14.0.1.3.0",
+    "version": "14.0.1.4.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "summary": "Import RFQ or sale orders from files",
