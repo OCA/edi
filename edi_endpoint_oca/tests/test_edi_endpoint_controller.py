08c9caafcdee4d8f5c7816a5db6d82379b4e30f5
Simone Orsi <simahawk@gmail.com>
2022-06-15 21:40:36 +0200

    edi_endpoint: integrate e_route_handler xworker fix
    Former version of `endpoint_route_handler` had a major flaw:
    
    routing rule registry was not properly shared across workers
    forcing us to restart the instance to make sure all envs were inline w/
    it.
    
    This change adapts edi_endpoint_oca to the new version which contains
    some refactoring.

--- edi_endpoint_oca/tests/test_edi_endpoint_controller.py
+++ edi_endpoint_oca/tests/test_edi_endpoint_controller.py
@@ -5,13 +5,16 @@
 import os
 import unittest
 
-from odoo.tests.common import HttpCase
+from odoo.tests.common import HttpSavepointCase
 
 
 @unittest.skipIf(os.getenv("SKIP_HTTP_CASE"), "EDIEndpointHttpCase skipped")
-class EDIEndpointHttpCase(HttpCase):
-    def setUp(self):
-        super().setUp()
+class EDIEndpointHttpCase(HttpSavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # force sync for demo records
+        cls.env["edi.endpoint"].search([])._handle_registry_sync()
 
     def test_call1(self):
         response = self.url_open("/edi/demo/try")
