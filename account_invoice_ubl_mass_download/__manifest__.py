d3f9f27146934154ffbefe92a3695f0ea51057d6
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 11:24:14 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- account_invoice_ubl_mass_download/__manifest__.py
+++ account_invoice_ubl_mass_download/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Invoice UBL Mass Download",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Coop IT Easy SCRLfs",
+    "author": "Coop IT Easy SC",
     "website": "https://coopiteasy.be",
     "depends": ["account_invoice_ubl"],
     "data": ["data/ir_actions_server.xml", "data/ir_cron.xml"],
--- account_invoice_ubl_mass_download/readme/CONTRIBUTORS.rst
+++ account_invoice_ubl_mass_download/readme/CONTRIBUTORS.rst
@@ -1,2 +1,2 @@
-* `Coop IT Easy SCRLfs <https://coopiteasy.be>`_:
+* `Coop IT Easy SC <https://coopiteasy.be>`_:
     * Rémy Taymans <remy@coopiteasy.be>
