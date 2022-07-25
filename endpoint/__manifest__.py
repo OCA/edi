940a0b5a51c42098fa4083f7012a9efea54ef19d
Simone Orsi <simahawk@gmail.com>
2022-07-25 17:26:30 +0200

    endpoint: use registry_sync flag and improve tests

--- endpoint/__manifest__.py
+++ endpoint/__manifest__.py
@@ -14,6 +14,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/endpoint_view.xml",
+        "data/server_action.xml",
     ],
     "demo": [
         "demo/endpoint_demo.xml",
