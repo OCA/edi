ac713da7aab71cf974b68da8ca7cd7c7e12da652
Simone Orsi <simahawk@gmail.com>
2022-07-28 15:33:42 +0200

    edi_exchange_template_oca: fix xml_purge_nswrapper
    Use `name()` because the real element could be namespaced on render
    leading to mismatch by element type.

--- edi_exchange_template_oca/utils.py
+++ edi_exchange_template_oca/utils.py
@@ -25,8 +25,9 @@ def xml_purge_nswrapper(xml_content):
     if not (xml_content and xml_content.strip()):
         return xml_content
     root = etree.XML(xml_content)
-    # deeper elements come after, keep the root element at the end (if any)
-    for nswrapper in reversed(root.xpath("//nswrapper")):
+    # Deeper elements come after, keep the root element at the end (if any).
+    # Use `name()` because the real element could be namespaced on render.
+    for nswrapper in reversed(root.xpath("//*[name() = 'nswrapper']")):
         parent = nswrapper.getparent()
         if parent is None:
             # fmt:off
