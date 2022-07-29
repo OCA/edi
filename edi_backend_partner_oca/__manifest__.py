8e9577b55131d1f03099af717216d17aa9bd0483
Jaume Bernaus <jaume.bernaus@forgeflow.com>
2022-07-29 16:01:32 +0200

    [MIG] edi_backend_partner_oca: Migration to 15.0

--- edi_backend_partner_oca/__manifest__.py
+++ edi_backend_partner_oca/__manifest__.py
@@ -2,9 +2,9 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "Edi Backend Partner",
-    "summary": "add the a partner field in EDI backend",
-    "version": "14.0.1.0.1",
+    "name": "EDI Backend Partner",
+    "summary": "Add the a partner field to EDI backend",
+    "version": "15.0.1.0.0",
     "license": "LGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "maintainers": ["LoisRForgeFlow"],
