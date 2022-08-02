bac6350da8bf24b8949e9ad9e5256fb684cedbda
Simone Orsi <simahawk@gmail.com>
2022-08-02 14:56:58 +0200

    Add edi_sale_oca

---
+++ edi_sale_oca/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EDI Sales",
+    "summary": """
+        Configuration and special behaviors for EDI on sales.
+    """,
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["sale", "edi_oca", "component_event"],
+    "data": [
+        "views/res_partner.xml",
+        "views/sale_order.xml",
+        "views/edi_exchange_record.xml",
+    ],
+}
