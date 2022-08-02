d072000a69cac05bfa1db735998180143ef8020e
Simone Orsi <simahawk@gmail.com>
2022-08-02 14:56:58 +0200

    Add edi_sale_oca

--- edi_sale_order_import/tests/test_process.py
+++ edi_sale_order_import/tests/test_process.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Camptocamp SA
 # @author: Simone Orsi <simahawk@gmail.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import base64
 import textwrap
