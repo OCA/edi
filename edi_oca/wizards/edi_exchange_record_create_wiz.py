597f8d880cf2134cf2209d4fc85e02836d111896
Simone Orsi <simahawk@gmail.com>
2022-08-11 16:33:33 +0200

    edi: improve consumer mixin helpers

--- edi_oca/wizards/edi_exchange_record_create_wiz.py
+++ edi_oca/wizards/edi_exchange_record_create_wiz.py
@@ -17,8 +17,9 @@ class EdiExchangeRecordCreateWiz(models.TransientModel):
     backend_id = fields.Many2one("edi.backend", required=True)
 
     def create_edi(self):
-        return (
-            self.env[self.model]
-            .browse(self.res_id)
-            ._edi_create_exchange_record(self.exchange_type_id, self.backend_id)
+        record = self.env[self.model].browse(self.res_id)
+        exchange_record = record._edi_create_exchange_record(
+            self.exchange_type_id, self.backend_id
         )
+        record._event("on_edi_generate_manual").notify(record, exchange_record)
+        return exchange_record.get_formview_action()
