c916530dae08c6747c32cab5d62584d481af6ff6
Simone Orsi <simahawk@gmail.com>
2022-08-12 15:01:17 +0200

    edi_endpoint: temp fix for tests
    I don't know why but  when test_edi_endpoint runs before these tests
    the rollback of the exception catched within the test `test_archive_check`
    make the controller lookup fail.
    Since the whole routing registry machinery is going to be refactored
    in https://github.com/OCA/edi/pull/633
    let's survive w/ this forced registration for now.

--- edi_endpoint_oca/tests/test_edi_endpoint_controller.py
+++ edi_endpoint_oca/tests/test_edi_endpoint_controller.py
@@ -12,6 +12,15 @@ from odoo.tests.common import HttpCase
 class EDIEndpointHttpCase(HttpCase):
     def setUp(self):
         super().setUp()
+        # I don't know why but  when test_edi_endpoint runs before these tests
+        # the rollback of the exception catched within the test `test_archive_check`
+        # make the controller lookup fail.
+        # Since the whole routing registry machinery is going to be refactored
+        # in https://github.com/OCA/edi/pull/633
+        # let's survive w/ this forced registration for now.
+        self.env.ref("edi_endpoint_oca.edi_endpoint_demo_1")._register_controllers(
+            init=True
+        )
 
     def test_call1(self):
         response = self.url_open("/edi/demo/try")
