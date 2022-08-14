6006d20eff118b456b1da91c87e2019a8555ad4b
Simone Orsi <simahawk@gmail.com>
2022-08-14 09:48:33 +0200

    edi_sale_order_import*: TODO deprecate

--- edi_sale_order_import/__manifest__.py
+++ edi_sale_order_import/__manifest__.py
@@ -2,6 +2,7 @@
 # @author: Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+# TODO: deprecate
 {
     "name": "EDI Sale order import",
     "summary": """Plug sale_order_import into EDI machinery.""",
--- edi_sale_order_import_ubl/__manifest__.py
+++ edi_sale_order_import_ubl/__manifest__.py
@@ -2,6 +2,7 @@
 # @author: Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+# TODO: deprecate
 {
     "name": "EDI Sale order import UBL",
     "summary": """Plug sale_order_import_ubl into EDI machinery.""",
