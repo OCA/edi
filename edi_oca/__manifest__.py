6c522b62c585dfd50701ab8cf5ad09a099818a51
Simone Orsi <simahawk@gmail.com>
2022-08-19 09:57:14 +0200

    edi_oca: bump 15.0.1.4.0

--- edi_oca/__manifest__.py
+++ edi_oca/__manifest__.py
@@ -9,7 +9,7 @@
     Define backends, exchange types, exchange records,
     basic automation and views for handling EDI exchanges.
     """,
-    "version": "15.0.1.3.3",
+    "version": "15.0.1.4.0",
     "website": "https://github.com/OCA/edi",
     "development_status": "Beta",
     "license": "LGPL-3",
