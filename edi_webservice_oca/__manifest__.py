9872272b9989bd0476bb242093d12202c1aaee01
Simone Orsi <simahawk@gmail.com>
2022-08-26 22:46:05 +0200

    edi_webservice: add generic send component

--- edi_webservice_oca/__manifest__.py
+++ edi_webservice_oca/__manifest__.py
@@ -1,15 +1,18 @@
 # Copyright 2020 Creu Blanca
+# Copyright 2022 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Edi WebService",
+    "name": "EDI WebService",
     "summary": """
         Defines webservice integration from EDI Exchange records""",
     "version": "14.0.1.2.0",
     "license": "AGPL-3",
     "development_status": "Beta",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "Creu Blanca, Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["etobella", "simahawk"],
     "website": "https://github.com/OCA/edi",
     "depends": ["edi_oca", "webservice"],
     "data": ["views/edi_backend.xml", "security/ir.model.access.csv"],
+    "demo": ["demo/edi_backend.xml"],
 }
