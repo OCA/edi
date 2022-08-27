8dda83f059b2d261dc079736e4ff26c026039eb0
Simone Orsi <simahawk@gmail.com>
2022-08-27 15:03:35 +0200

    webservice: improve call
    'call' now accepts these explicit kw params:
    
    * 'url': to override the request's url completely
    * 'url_params': to pass interpolation params for url formatting.

--- webservice/components/request_adapter.py
+++ webservice/components/request_adapter.py
@@ -13,14 +13,14 @@ class BaseRestRequestsAdapter(Component):
     _inherit = "base.webservice.adapter"
     _webservice_protocol = "http"
 
-    def _request(self, method, **kwargs):
+    # TODO: url and url_params could come from work_ctx
+    def _request(self, method, url=None, url_params=None, **kwargs):
+        url = self._get_url(url=url, url_params=url_params)
         new_kwargs = kwargs.copy()
         new_kwargs.update(
             {"auth": self._get_auth(**kwargs), "headers": self._get_headers(**kwargs)}
         )
-        request = requests.request(
-            method, self.collection.url.format(**kwargs), **new_kwargs
-        )
+        request = requests.request(method, url, **new_kwargs)
         request.raise_for_status()
         return request.content
 
@@ -47,3 +47,11 @@ class BaseRestRequestsAdapter(Component):
         if isinstance(headers, dict):
             result.update(headers)
         return result
+
+    def _get_url(self, url=None, url_params=None, **kwargs):
+        if not url:
+            # TODO: if url is given, we should validate the domain
+            # to avoid abusing a webservice backend for different calls.
+            url = self.collection.url
+        url_params = url_params or kwargs
+        return url.format(**url_params)
