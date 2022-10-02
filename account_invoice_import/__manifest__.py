988b5aee08625c3d1267a6b746ec247475a95461
Alexis de Lattre <alexis.delattre@akretion.com>
2022-10-03 00:37:38 +0200

    [MIG] account_invoice_import to v15

--- account_invoice_import/__manifest__.py
+++ account_invoice_import/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2015-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2015-2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Invoice Import",
-    "version": "14.0.3.0.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "Import supplier invoices/refunds as PDF or XML files",
