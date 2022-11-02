5d9e65779170587c7ce663c334e89bbf70f21da6
sergio-teruel <sergio.teruel@tecnativa.com>
2022-11-02 23:33:33 +0100

    [MIG] edi_voxel_sale_order_import: Migration to v15.0

---
+++ edi_voxel_sale_order_import_oca/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2019 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Voxel sale order oca",
+    "summary": "Import sale order from Voxel.",
+    "version": "15.0.1.0.0",
+    "category": "Sale",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "license": "AGPL-3",
+    "depends": [
+        "edi_voxel_oca",
+        "onchange_helper",  # See server-side forms (SSF) on v12
+        "sale",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/voxel_sale_order_security.xml",
+        "data/ir_cron_data.xml",
+        "views/res_company_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
