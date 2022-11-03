931fe989b00c5c2a634c893e23603659d4899416
sergio-teruel <sergio.teruel@tecnativa.com>
2022-11-03 15:49:23 +0100

    [MIG] edi_voxel_stock_picking_secondary_unit: Migration to v15.0

---
+++ edi_voxel_stock_picking_secondary_unit_oca/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Voxel stock picking secondary unit",
+    "summary": "Export Secondary UoMs Voxel Code in picking Voxel documents",
+    "version": "15.0.1.0.0",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/edi",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["ernestotejeda"],
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock_secondary_unit",
+        "edi_voxel_secondary_unit_oca",
+        "edi_voxel_stock_picking_oca",
+    ],
+}
