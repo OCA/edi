1627274bb922af78bc21bf3ac702634cce3f38c8
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-26 23:13:39 +0100

    [MIG] account_invoice_facturx to v16

--- account_invoice_facturx/hooks.py
+++ account_invoice_facturx/hooks.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2018-2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,5 +7,7 @@ from odoo import SUPERUSER_ID, api
 
 def set_xml_format_in_pdf_invoice_to_facturx(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    companies = env["res.company"].search([])
+    companies = env["res.company"].search(
+        [("xml_format_in_pdf_invoice", "in", (False, "none"))]
+    )
     companies.write({"xml_format_in_pdf_invoice": "factur-x"})
