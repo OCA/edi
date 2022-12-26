74ac350c4ca1401cf76607ed0d971cdf7a2013cf
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-26 23:19:24 +0100

    [MIG] account_invoice_facturx_py3o to v16

--- account_invoice_facturx_py3o/__manifest__.py
+++ account_invoice_facturx_py3o/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2017-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2017-2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Py3o Factur-x Invoice",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Invoicing Management",
     "license": "AGPL-3",
     "summary": "Generate Factur-X invoices with Py3o reporting engine",
@@ -12,6 +12,6 @@
     "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/edi",
     "depends": ["account_invoice_facturx", "report_py3o"],
-    "external_dependencies": {"python": ["factur-x"]},
     "installable": True,
+    "auto_install": True,
 }
--- account_invoice_facturx_py3o/models/py3o_report.py
+++ account_invoice_facturx_py3o/models/py3o_report.py
@@ -1,17 +1,8 @@
-# Copyright 2017-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2017-2022 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
-
 from odoo import models
-
-logger = logging.getLogger(__name__)
-
-try:
-    from facturx import generate_facturx_from_file
-except ImportError:
-    logger.debug("Cannot import facturx")
 
 
 class Py3oReport(models.TransientModel):
@@ -28,16 +19,8 @@ class Py3oReport(models.TransientModel):
             and self.ir_actions_report_id.py3o_filetype == "pdf"
             and result_path
         ):
-            invoice = model_instance
+            move = model_instance
             # re-write PDF on result_path
-            if invoice._xml_format_in_pdf_invoice() == "factur-x":
-                facturx_xml_str, level = invoice.generate_facturx_xml()
-                pdf_metadata = invoice._prepare_pdf_metadata()
-                generate_facturx_from_file(
-                    result_path,
-                    facturx_xml_str,
-                    check_xsd=False,
-                    facturx_level=level,
-                    pdf_metadata=pdf_metadata,
-                )
+            if move._xml_format_in_pdf_invoice() == "factur-x":
+                move.regular_pdf_invoice_to_facturx_invoice(result_path)
         return super()._postprocess_report(model_instance, result_path)
