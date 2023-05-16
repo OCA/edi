0f90062bbaaf3d554025df69120e1429cb388d34
Florent Xicluna <florent.xicluna@camptocamp.com>
2023-05-16 15:10:52 +0200

    edi_oca: avoid backend mismatch

--- edi_oca/tests/test_exchange_type.py
+++ edi_oca/tests/test_exchange_type.py
@@ -23,6 +23,25 @@ class EDIExchangeTypeTestCase(EDIBackendCommonTestCase):
             self.exchange_type_out_ack.ack_for_type_ids.ids,
         )
 
+    def test_same_code_same_backend(self):
+        with self.assertRaises(Exception) as err:
+            self.exchange_type_in.copy({"code": "test_csv_input"})
+        err_msg = err.exception.args[0]
+        self.assertTrue(
+            err_msg.startswith("duplicate key value violates unique constraint")
+        )
+
+    def test_same_code_different_backend(self):
+        new_backend = self.backend.copy()
+        new_type = self.exchange_type_in.copy(
+            {"backend_id": new_backend.id, "code": "test_csv_input"}
+        )
+        self.assertEqual(new_type.code, self.exchange_type_in.code)
+        self.assertEqual(
+            new_type.backend_type_id, self.exchange_type_in.backend_type_id
+        )
+        self.assertNotEqual(new_type.backend_id, self.exchange_type_in.backend_id)
+
     def test_advanced_settings(self):
         settings = """
         components:
