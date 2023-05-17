9eb4c155440da66cf0bd2b5a31a4d038747b2354
Florent Xicluna <florent.xicluna@camptocamp.com>
2023-05-17 07:46:10 +0200

    edi_oca: assertion was not executed

--- edi_oca/tests/test_record.py
+++ edi_oca/tests/test_record.py
@@ -35,16 +35,14 @@ class EDIRecordTestCase(EDIBackendCommonTestCase):
         self.assertNotEqual(new_record.identifier, record.identifier)
 
     def test_record_validate_state(self):
-        with self.assertRaises(exceptions.ValidationError) as err:
+        expected_err = "Exchange state must respect direction!"
+        with self.assertRaises(exceptions.ValidationError, msg=expected_err):
             vals = {
                 "model": self.partner._name,
                 "res_id": self.partner.id,
                 "edi_exchange_state": "output_pending",
             }
             self.backend.create_record("test_csv_input", vals)
-            self.assertEqual(
-                err.exception.name, "Exchange state must respect direction!"
-            )
 
     def test_record_exchange_date(self):
         vals = {
