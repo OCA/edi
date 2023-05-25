3144d2cda0375263cfa537dc28a6a6e46efae406
Jacques-Etienne Baudoux <je@bcim.be>
2023-05-25 12:23:32 +0200

    [FIX] base_ubl: schemeID for GTIN is 0160

--- base_ubl/models/ubl.py
+++ base_ubl/models/ubl.py
@@ -371,7 +371,7 @@ class BaseUbl(models.AbstractModel):
                     item, ns['cac'] + 'StandardItemIdentification')
                 std_identification_id = etree.SubElement(
                     std_identification, ns['cbc'] + 'ID',
-                    schemeAgencyID='6', schemeID='GTIN')
+                    schemeAgencyID='6', schemeID='0160')  # GTIN = 0160
                 std_identification_id.text = product.barcode
             # I'm not 100% sure, but it seems that ClassifiedTaxCategory
             # contains the taxes of the product without taking into
@@ -652,9 +652,14 @@ class BaseUbl(models.AbstractModel):
         return {}
 
     def ubl_parse_product(self, line_node, ns):
+        # GTIN schemeID is 0160
         barcode_xpath = line_node.xpath(
-            "cac:Item/cac:StandardItemIdentification/cbc:ID[@schemeID='GTIN']",
+            "cac:Item/cac:StandardItemIdentification/cbc:ID[@schemeID='0160']",
             namespaces=ns)
+        if not barcode_xpath:
+            barcode_xpath = line_node.xpath(
+                "cac:Item/cac:StandardItemIdentification/cbc:ID[@schemeID='GTIN']",
+                namespaces=ns)
         code_xpath = line_node.xpath(
             "cac:Item/cac:SellersItemIdentification/cbc:ID", namespaces=ns)
         product_dict = {
