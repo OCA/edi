47353b38cd2596e33693093f30fbedd6f061307d
Simone Orsi <simahawk@gmail.com>
2023-06-22 12:14:50 +0200

    edi_sale_ubl: improve line state test

--- edi_sale_ubl_oca/tests/test_order_state.py
+++ edi_sale_ubl_oca/tests/test_order_state.py
@@ -40,14 +40,65 @@ class TestOrderInbound(SavepointCase, EDIBackendTestMixin, OrderMixin):
 
     def test_state_partially_accepted(self):
         order = self.sale
-        order.order_line[0].product_uom_qty = order.order_line[0].product_uom_qty - 1
+        orig_qties = {}
+        for line in order.order_line:
+            orig_qties[line.id] = line.product_uom_qty
+        line1, line2, line3 = order.order_line
+        # change line 1
+        line1.product_uom_qty = orig_qties[line1.id] - 1
         order._edi_update_state()
         self.assertEqual(
             order.edi_state_id.code, order.EDI_STATE_ORDER_CONDITIONALLY_ACCEPTED
         )
         self.assertTrue(
-            order.mapped("order_line.edi_state_id").code,
-            order.EDI_STATE_ORDER_LINE_CHANGED,
+            order.mapped("order_line.edi_state_id.code"),
+            [
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+                order.EDI_STATE_ORDER_LINE_ACCEPTED,
+                order.EDI_STATE_ORDER_LINE_ACCEPTED,
+            ],
+        )
+        # change line 2
+        line2.product_uom_qty = orig_qties[line2.id] - 1
+        order._edi_update_state()
+        self.assertEqual(
+            order.edi_state_id.code, order.EDI_STATE_ORDER_CONDITIONALLY_ACCEPTED
+        )
+        self.assertTrue(
+            order.mapped("order_line.edi_state_id.code"),
+            [
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+                order.EDI_STATE_ORDER_LINE_ACCEPTED,
+            ],
+        )
+        # change line 3
+        line3.product_uom_qty = orig_qties[line3.id] - 1
+        order._edi_update_state()
+        self.assertEqual(
+            order.edi_state_id.code, order.EDI_STATE_ORDER_CONDITIONALLY_ACCEPTED
+        )
+        self.assertTrue(
+            order.mapped("order_line.edi_state_id.code"),
+            [
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+            ],
+        )
+        # restore line 1
+        line1.product_uom_qty = orig_qties[line1.id]
+        order._edi_update_state()
+        self.assertEqual(
+            order.edi_state_id.code, order.EDI_STATE_ORDER_CONDITIONALLY_ACCEPTED
+        )
+        self.assertTrue(
+            order.mapped("order_line.edi_state_id.code"),
+            [
+                order.EDI_STATE_ORDER_LINE_ACCEPTED,
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+                order.EDI_STATE_ORDER_LINE_CHANGED,
+            ],
         )
 
     def test_state_rejected(self):
@@ -55,3 +106,45 @@ class TestOrderInbound(SavepointCase, EDIBackendTestMixin, OrderMixin):
         order.action_cancel()
         order._edi_update_state()
         self.assertEqual(order.edi_state_id.code, order.EDI_STATE_ORDER_REJECTED)
+
+    def test_state_accepted_add_line(self):
+        order = self.sale
+        order.write({"state": "sale"})
+        self.assertEqual(order.edi_state_id.code, order.EDI_STATE_ORDER_ACCEPTED)
+        self.assertTrue(
+            order.mapped("order_line.edi_state_id").code,
+            order.EDI_STATE_ORDER_LINE_ACCEPTED,
+        )
+        lines = order.order_line
+        order.write(
+            {
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product_a.id,
+                            "product_uom_qty": 300,
+                            "edi_id": 4000,
+                            "origin_exchange_record_id": self.exc_record_in.id,
+                        },
+                    )
+                ]
+            }
+        )
+        new_line = order.order_line - lines
+        self.assertEqual(
+            new_line.edi_state_id.code, order.EDI_STATE_ORDER_LINE_ACCEPTED
+        )
+        new_line = order.order_line.create(
+            {
+                "order_id": order.id,
+                "product_id": self.product_a.id,
+                "product_uom_qty": 300,
+                "edi_id": 4000,
+                "origin_exchange_record_id": self.exc_record_in.id,
+            }
+        )
+        self.assertEqual(
+            new_line.edi_state_id.code, order.EDI_STATE_ORDER_LINE_ACCEPTED
+        )
