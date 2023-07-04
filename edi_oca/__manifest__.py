32b72e60277c4e2e04abddcbed9899d75aecb5c4
Simone Orsi <simahawk@gmail.com>
2023-07-04 17:53:59 +0200

    edi: refactor model rules w/ specific model
    Long awaited improvement.
    Allows to decouple rules by model giving much more flexibility.
    Migration steps included.

--- edi_oca/__manifest__.py
+++ edi_oca/__manifest__.py
@@ -35,6 +35,7 @@
         "views/edi_backend_type_views.xml",
         "views/edi_exchange_record_views.xml",
         "views/edi_exchange_type_views.xml",
+        "views/edi_exchange_type_rule_views.xml",
         "views/menuitems.xml",
         "templates/exchange_chatter_msg.xml",
         "templates/exchange_mixin_buttons.xml",
