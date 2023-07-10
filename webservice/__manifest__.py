c4b055429c1ade779dc568108235f99dd5ff9aad
EvaSForgeFlow <eva.m.szlifierz@forgeflow.com>
2023-07-10 09:56:58 +0200

    [MIG] webservice: Migration to 16.0

--- webservice/__manifest__.py
+++ webservice/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "WebService",
     "summary": """
         Defines webservice abstract definition to be used generally""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
     "maintainers": ["etobella"],
