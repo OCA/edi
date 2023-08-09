80c94ddfb4a48a7e6c718cede6ac05ad7159499f
Simone Orsi <simahawk@gmail.com>
2023-08-09 17:37:46 +0200

    edi: refactor model rules backward compat

--- edi_oca/tests/__init__.py
+++ edi_oca/tests/__init__.py
@@ -12,3 +12,4 @@ from . import test_consumer_mixin
 from . import test_edi_backend_cron
 from . import test_security
 from . import test_quick_exec
+from . import test_exchange_type_deprecated_fields
