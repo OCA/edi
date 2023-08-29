8c25ed949eb51c965b41a7c9835359eb5cf20836
Gediminas Venclova <gediminasvenc@gmail.com>
2023-08-29 10:14:20 +0300

    [FIX] base_business_document_import: fixed typo in _match_tax method

--- base_business_document_import/__manifest__.py
+++ base_business_document_import/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Base Business Document Import",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Tools",
     "license": "AGPL-3",
     "summary": "Provides technical tools to import sale orders or supplier invoices",
