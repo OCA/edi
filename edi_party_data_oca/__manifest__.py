63b10d613a09faa778ce9f1f07cf31c70e75aae5
duongtq <duongtq@trobz.com>
2023-09-12 10:47:21 +0700

    [MIG] edi_party_data_oca: Migration to 16.0

--- edi_party_data_oca/__manifest__.py
+++ edi_party_data_oca/__manifest__.py
@@ -1,13 +1,12 @@
 # Copyright 2022 Camptocamp SA
 # @author: Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     "name": "EDI Party data",
     "summary": """
     Allow to configure and retrieve party information for EDI exchanges.
     """,
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "development_status": "Alpha",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/edi",
