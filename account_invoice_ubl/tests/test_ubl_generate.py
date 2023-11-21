85a70659c703efbb0007bd0e3c73313aae341805
Simone Orsi <simahawk@gmail.com>
2023-11-21 13:21:18 +0100

    account_invoice_ubl: fix tests and split common

--- account_invoice_ubl/tests/test_ubl_generate.py
+++ account_invoice_ubl/tests/test_ubl_generate.py
@@ -3,90 +3,29 @@
 # Copyright 2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase, tagged
+from odoo.tests.common import HttpSavepointCase, tagged
 from odoo.tools import mute_logger
 
 from ..hooks import (
     remove_ubl_xml_format_in_pdf_invoice,
     set_xml_format_in_pdf_invoice_to_ubl,
 )
+from .common import TestUblInvoiceMixin
 
 MUTE_LOGGER = "odoo.addons.account_invoice_ubl.models.account_move"
 
+# Use http case to make PDF rendering work
+
 
 @tagged("-at_install", "post_install")
-class TestUblInvoice(SavepointCase):
+class TestUblInvoice(HttpSavepointCase, TestUblInvoiceMixin):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
 
-    def test_only_create_invoice(
-        self, product=False, qty=1, price=12.42, discount=0, validate=True
-    ):
-        aio = self.env["account.move"]
-        ato = self.env["account.tax"]
-        company = self.env.ref("base.main_company")
-        taxes = ato.search(
-            [
-                ("company_id", "=", company.id),
-                ("type_tax_use", "=", "sale"),
-                ("unece_type_id", "!=", False),
-                ("unece_categ_id", "!=", False),
-                ("amount_type", "=", "percent"),
-            ]
-        )
-        if taxes:
-            tax = taxes[0]
-        else:
-            unece_type_id = self.env.ref("account_tax_unece.tax_type_vat").id
-            unece_categ_id = self.env.ref("account_tax_unece.tax_categ_s").id
-            tax = ato.create(
-                {
-                    "name": "German VAT purchase 18.0%",
-                    "description": "DE-VAT-sale-18.0",
-                    "company_id": company.id,
-                    "type_tax_use": "sale",
-                    "price_include": False,
-                    "amount": 18,
-                    "amount_type": "percent",
-                    "unece_type_id": unece_type_id,
-                    "unece_categ_id": unece_categ_id,
-                }
-            )
-        # validate invoice
-        if not product:
-            product = self.env.ref("product.product_product_4")
-        invoice = aio.create(
-            {
-                "partner_id": self.env.ref("base.res_partner_2").id,
-                "currency_id": self.env.ref("base.EUR").id,
-                "move_type": "out_invoice",
-                "company_id": company.id,
-                "name": "SO1242",
-                "invoice_line_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "product_id": product.id,
-                            "product_uom_id": product.uom_id.id,
-                            "quantity": qty,
-                            "price_unit": price,
-                            "discount": discount,
-                            "name": product.name,
-                            "tax_ids": [(6, 0, [tax.id])],
-                        },
-                    )
-                ],
-            }
-        )
-        if validate:
-            invoice.action_post()
-        return invoice
-
     def test_ubl_generate(self):
-        invoice = self.test_only_create_invoice()
+        invoice = self._create_invoice()
         if invoice.company_id.xml_format_in_pdf_invoice != "ubl":
             invoice.company_id.xml_format_in_pdf_invoice = "ubl"
         for version in ["2.0", "2.1"]:
@@ -100,7 +39,7 @@ class TestUblInvoice(SavepointCase):
             self.assertTrue(invoice_filename in res)
 
     def test_attach_ubl_xml_file_button(self):
-        invoice = self.test_only_create_invoice()
+        invoice = self._create_invoice()
         if invoice.company_id.xml_format_in_pdf_invoice != "ubl":
             invoice.company_id.xml_format_in_pdf_invoice = "ubl"
         self.assertFalse(invoice.company_id.embed_pdf_in_ubl_xml_invoice)
