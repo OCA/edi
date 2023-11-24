fc0780a30da23ce1faa3b1e0d1295ef572e2cb8f
Simone Orsi <simahawk@gmail.com>
2023-11-24 16:05:46 +0100

    edi: use job identity_key
    Prevents having duplicated jobs for the same record as far as possible.

--- edi_oca/utils.py
+++ edi_oca/utils.py
@@ -5,6 +5,7 @@
 import hashlib
 
 from odoo.addons.http_routing.models.ir_http import slugify
+from odoo.addons.queue_job.job import identity_exact_hasher
 
 
 def normalize_string(a_string, sep="_"):
@@ -14,3 +15,12 @@ def normalize_string(a_string, sep="_"):
 
 def get_checksum(filecontent):
     return hashlib.md5(filecontent).hexdigest()
+
+
+def exchange_record_job_identity_exact(job_):
+    hasher = identity_exact_hasher(job_)
+    # Include files checksum
+    hasher.update(
+        str(sorted(job_.recordset.mapped("exchange_filechecksum"))).encode("utf-8")
+    )
+    return hasher.hexdigest()
