378acd2499ee0103da10ec07b880f9dc96963d55
Simone Orsi <simahawk@gmail.com>
2023-11-24 16:00:00 +0100

    edi: add file checksum

--- edi_oca/utils.py
+++ edi_oca/utils.py
@@ -2,9 +2,15 @@
 # @author Simone Orsi <simahawk@gmail.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import hashlib
+
 from odoo.addons.http_routing.models.ir_http import slugify
 
 
 def normalize_string(a_string, sep="_"):
     """Normalize given string, replace dashes with given separator."""
     return slugify(a_string).replace("-", sep)
+
+
+def get_checksum(filecontent):
+    return hashlib.md5(filecontent).hexdigest()
