3fbebf7dd448c9ed69f4051fe79e9d96592addcc
thien <thienvh@trobz.com>
2023-11-30 17:25:18 +0700

    [IMP] despatch_advice_import: pre-commit stuff

--- despatch_advice_import/__manifest__.py
+++ despatch_advice_import/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Despatch Advice Import',
-    'summary': """
+    "name": "Despatch Advice Import",
+    "summary": """
         Despatch Advice import""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'depends': ['purchase', 'base_business_document_import_stock'],
-    'data': ['wizard/despatch_advice_import.xml'],
-    'demo': [],
-    'installable': True,
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA/edi",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "depends": ["purchase", "base_business_document_import_stock"],
+    "data": ["wizard/despatch_advice_import.xml"],
+    "demo": [],
+    "installable": True,
 }
