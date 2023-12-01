dec96413614abd4b7f7c378ed57c7db56b31fadc
thien <thienvh@trobz.com>
2023-12-01 15:51:11 +0700

    [MIG] despatch_advice_import_ubl: Migration to 16.0

--- despatch_advice_import_ubl/__manifest__.py
+++ despatch_advice_import_ubl/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Despatch Advice Import Ubl",
     "summary": """
         Import Despatch Advice files""",
-    "version": "10.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
--- despatch_advice_import_ubl/tests/test_despatch_advice_import.py
+++ despatch_advice_import_ubl/tests/test_despatch_advice_import.py
@@ -1,15 +1,17 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from base64 import b64decode, b64encode
+
 from odoo import fields
-from odoo.tests import SavepointCase
+from odoo.tests.common import TransactionCase
 from odoo.tools import file_open
 
 
-class TestDespatchAdviceImport(SavepointCase):
+class TestDespatchAdviceImport(TransactionCase):
     @classmethod
     def setUpClass(cls):
-        super(TestDespatchAdviceImport, cls).setUpClass()
+        super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.supplier = cls.env.ref("base.res_partner_12")
         cls.supplier.vat = "BE0477472701"
@@ -18,14 +20,18 @@ class TestDespatchAdviceImport(SavepointCase):
             {
                 "name": "Product 1",
                 "default_code": "1234567",
-                "seller_ids": [(0, 0, {"name": cls.supplier.id, "product_code": "P1"})],
+                "seller_ids": [
+                    (0, 0, {"partner_id": cls.supplier.id, "product_code": "P1"})
+                ],
             }
         )
         cls.product_2 = cls.env["product.product"].create(
             {
                 "name": "Product 2",
                 "default_code": "2345678",
-                "seller_ids": [(0, 0, {"name": cls.supplier.id, "product_code": "P2"})],
+                "seller_ids": [
+                    (0, 0, {"partner_id": cls.supplier.id, "product_code": "P2"})
+                ],
             }
         )
         cls.purchase_order = cls.env["purchase.order"].create(
@@ -42,7 +48,7 @@ class TestDespatchAdviceImport(SavepointCase):
                 "name": cls.product_2.name,
                 "date_planned": fields.Datetime.now(),
                 "product_qty": 24,
-                "product_uom": cls.env.ref("product.product_uom_unit").id,
+                "product_uom": cls.env.ref("uom.product_uom_unit").id,
                 "price_unit": 15,
             }
         )
@@ -53,12 +59,12 @@ class TestDespatchAdviceImport(SavepointCase):
                 "name": cls.product_2.name,
                 "date_planned": fields.Datetime.now(),
                 "product_qty": 15,
-                "product_uom": cls.env.ref("product.product_uom_unit").id,
+                "product_uom": cls.env.ref("uom.product_uom_unit").id,
                 "price_unit": 25,
             }
         )
         cls.DespatchAdviceImport = cls.env["despatch.advice.import"]
-        cls.ProcurementOrder = cls.env["procurement.order"]
+        # cls.ProcurementOrder = cls.env["procurement.order"]
 
         with file_open(
             "despatch_advice_import_ubl/tests/files/despatch_advice_tmpl.xml", "rb"
@@ -70,7 +76,7 @@ class TestDespatchAdviceImport(SavepointCase):
         ) as f:
             cls.despatch_advice_xml2 = f.read()
 
-    def test_01(self):
+    def test_xml_convert_to_internal_data_01(self):
         """
         Data:
             An UBL DespatchAdvice with all the information expected by the
@@ -80,7 +86,7 @@ class TestDespatchAdviceImport(SavepointCase):
         Expected result:
             All the fields are filled into the internal data structure.
         """
-        xml_content = self.despatch_advice_xml1.format(
+        xml_content = self.despatch_advice_xml1.decode("utf-8").format(
             order_id=self.purchase_order.name,
             line_1_id=self.line1.id,
             line_1_qty=self.line1.product_qty,
@@ -91,8 +97,9 @@ class TestDespatchAdviceImport(SavepointCase):
             line_2_product_ref=self.product_2.default_code,
             line_2_backorder_qty=0,
         )
+        xml_content = b64encode(xml_content.encode("utf-8"))
         result = self.DespatchAdviceImport.parse_despatch_advice(
-            xml_content, "test.xml"
+            b64decode(xml_content), "test.xml"
         )
         attachments = result.pop("attachments")
         self.assertTrue(attachments.get("test.xml"))
@@ -128,9 +135,9 @@ class TestDespatchAdviceImport(SavepointCase):
             "supplier": {"vat": "BE0477472701"},
         }
         self.maxDiff = None
-        self.assertDictEqual(expected, result)
+        self.assertEqual(expected, result)
 
-    def test_02(self):
+    def test_xml_convert_to_internal_data_02(self):
         """
         Data:
             An UBL DespatchAdvice with all the information expected by the
@@ -140,7 +147,7 @@ class TestDespatchAdviceImport(SavepointCase):
         Expected result:
             All the fields are filled into the internal data structure.
         """
-        xml_content = self.despatch_advice_xml2.format(
+        xml_content = self.despatch_advice_xml2.decode("utf-8").format(
             line_1_id=self.line1.id,
             line_1_order_id=self.purchase_order.name,
             line_1_qty=self.line1.product_qty,
@@ -152,8 +159,9 @@ class TestDespatchAdviceImport(SavepointCase):
             line_2_product_ref=self.product_2.default_code,
             line_2_backorder_qty=0,
         )
+        xml_content = b64encode(xml_content.encode("utf-8"))
         result = self.DespatchAdviceImport.parse_despatch_advice(
-            xml_content, "test2.xml"
+            b64decode(xml_content), "test2.xml"
         )
         attachments = result.pop("attachments")
         self.assertTrue(attachments.get("test2.xml"))
@@ -189,4 +197,4 @@ class TestDespatchAdviceImport(SavepointCase):
             "supplier": {"vat": "BE0477472701"},
         }
         self.maxDiff = None
-        self.assertDictEqual(expected, result)
+        self.assertEqual(expected, result)
