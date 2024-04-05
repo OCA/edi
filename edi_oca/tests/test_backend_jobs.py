52abc96fb98e8c996980cde0c6444ac2251723e7
Florent Xicluna <florent.xicluna@camptocamp.com>
2024-04-05 09:12:48 +0200

    edi_oca: Do not retry exchange_send indefinitely

--- edi_oca/tests/test_backend_jobs.py
+++ edi_oca/tests/test_backend_jobs.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import mock
+from freezegun import freeze_time
 from requests.exceptions import ConnectionError as ReqConnectionError
 
 from odoo.addons.queue_job.exception import RetryableJobError
@@ -75,6 +76,38 @@ class EDIBackendTestJobsCase(EDIBackendCommonTestCase, JobMixin):
             mocked.side_effect = ReqConnectionError("Connection broken")
             with self.assertRaises(RetryableJobError):
                 job.perform()
+        self.assertEqual(record.edi_exchange_state, "output_pending")
+
+    def test_output_fail_too_many_retries(self):
+        job_counter = self.job_counter()
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+            "edi_exchange_state": "output_pending",
+        }
+        record = self.backend.create_record("test_csv_output", vals)
+        record._write({"create_date": "2024-01-10 09:00:00"})
+        record._set_file_content("ABC")
+        with mock.patch.object(type(self.backend), "_exchange_send") as mocked:
+            mocked.side_effect = ReqConnectionError("Connection broken")
+            with freeze_time("2024-01-10 11:00:00"):
+                # + 2 hours
+                job = self.backend.with_delay().exchange_send(record)
+                with self.assertRaises(RetryableJobError):
+                    job.perform()
+            with freeze_time("2024-01-11 08:50:00"):
+                # + 23 hours and 50 minutes
+                job = self.backend.with_delay().exchange_send(record)
+                with self.assertRaises(RetryableJobError):
+                    job.perform()
+            self.assertEqual(record.edi_exchange_state, "output_pending")
+            with freeze_time("2024-01-11 09:20:00"):
+                # + 24 hours and 20 minutes
+                job = self.backend.with_delay().exchange_send(record)
+                res = job.perform()
+            self.assertIn("Error", res)
+        job_counter.search_created()
+        self.assertEqual(record.edi_exchange_state, "output_error_on_send")
 
     def test_input(self):
         job_counter = self.job_counter()
