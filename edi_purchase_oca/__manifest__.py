fdfeb3dd5bda9d58db1c57dfa0d2a0d570ea18b7
thien <thienvh@trobz.com>
2024-04-22 14:26:05 +0700

    [BPRT] edi_purchase_oca: Backport from 14.0

--- edi_purchase_oca/__manifest__.py
+++ edi_purchase_oca/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "EDI Purchase",
     "summary": """
         Define EDI Configuration for Purchase Orders""",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "LGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
