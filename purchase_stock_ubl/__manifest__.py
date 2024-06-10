4cb6c871530abbffc8d738e20e6b594672d0235a
Wodran Van de Sande <wodran.vandesande@dynapps.be>
2024-06-10 12:17:42 +0200

    [MIG] purchase_stock_ubl: Migration to 17.0

--- purchase_stock_ubl/__manifest__.py
+++ purchase_stock_ubl/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Purchase Stock UBL",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Purchase Management",
     "license": "AGPL-3",
     "summary": "Glue module for Purchase Order UBL and Stock/Inventory",
