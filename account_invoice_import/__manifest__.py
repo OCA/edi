42bbc7cbf051d3136f4c878c98331c6360506386
Alexis de Lattre <alexis.delattre@akretion.com>
2024-07-25 14:38:19 +0200

    [IMP] account_invoice_import: add support for discount
    Add support for discount on lines in the multi-line create scenario

--- account_invoice_import/__manifest__.py
+++ account_invoice_import/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Invoice Import",
-    "version": "14.0.3.3.2",
+    "version": "14.0.3.4.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "Import supplier invoices/refunds as PDF or XML files",
