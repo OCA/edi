909ff8b3955ed451807ff59b4e72194366c2c825
thien <thienvh@trobz.com>
2024-08-08 16:51:19 +0700

    edi_sale_order_import_ubl_endpoint: DEPRECATED

--- edi_sale_order_import_ubl_endpoint/__manifest__.py
+++ edi_sale_order_import_ubl_endpoint/__manifest__.py
@@ -1,17 +1,20 @@
 # Copyright 2021 Camptocamp SA
 # @author: Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-# TODO: rename to `*_example`
 {
     "name": "EDI Sale order import UBL endpoint",
-    "summary": """Provide a default endpoint to import SO in UBL format.""",
+    "summary": """
+        MODULE DEPRECATED: use `edi_sale_ubl_endpoint_oca`.
+
+    Plug sale_order_import_ubl_endpoint into EDI machinery.
+    """,
     "version": "14.0.1.1.0",
     "development_status": "Alpha",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/edi",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainers": ["simahawk"],
-    "depends": ["edi_sale_order_import", "sale_order_import_ubl", "edi_endpoint_oca"],
-    "auto_install": True,
-    "data": ["data/endpoint.xml"],
+    "depends": ["edi_sale_ubl_endpoint_oca"],
+    "auto_install": False,
+    "data": [],
 }
