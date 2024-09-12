e31e02ba9e15e8d109327ab4f7413e233d573567
Thierry Ducrest <thierry@ducrest.net>
2024-09-12 14:23:03 +0200

    [MIG] account_invoice_export_job: Migration to 17.0

--- account_invoice_export_job/__manifest__.py
+++ account_invoice_export_job/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Invoice Export Job",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Invoicing Management",
     "license": "AGPL-3",
     "summary": "",
