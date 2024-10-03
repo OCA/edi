bf3d2c3b6b00f1bb733677fad4f4aeaaa489d72e
Víctor Martínez <victor.martinez@tecnativa.com>
2024-10-03 10:06:38 +0200

    [MIG] base_import_pdf_by_template: Migration to 17.0
    TT50003

--- base_import_pdf_by_template/tests/test_base_import_pdf_by_template.py
+++ base_import_pdf_by_template/tests/test_base_import_pdf_by_template.py
@@ -5,36 +5,29 @@ from base64 import b64encode
 from os import path
 
 from odoo.exceptions import UserError
-from odoo.tests import Form, common, new_test_user
+from odoo.tests import Form, new_test_user
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestBaseImportPdfByTemplate(common.TransactionCase):
+class TestBaseImportPdfByTemplate(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(
-            context=dict(
-                cls.env.context,
-                mail_create_nolog=True,
-                mail_create_nosubscribe=True,
-                mail_notrack=True,
-                no_reset_password=True,
-                tracking_disable=True,
-            )
-        )
         cls.user = new_test_user(cls.env, login="test-user")
         cls.country_line = cls.env.ref(
             "base_import_pdf_by_template.demo_base_import_pdf_template_res_partner_line_03"
         )
         country_es = cls.env.ref("base.es")
-        cls.country_line.default_value = "%s,%s" % (country_es._name, country_es.id)
+        cls.country_line.default_value = f"{country_es._name},{country_es.id}"
         user_line = cls.env.ref(
             "base_import_pdf_by_template.demo_base_import_pdf_template_res_partner_header_04"
         )
-        user_line.fixed_value = "%s,%s" % (cls.user._name, cls.user.id)
+        user_line.fixed_value = f"{cls.user._name},{cls.user.id}"
         cls.template = cls.env.ref(
             "base_import_pdf_by_template.demo_base_import_pdf_template_res_partner"
         )
+        cls.env["res.partner.industry"].create({"name": "Food"})
 
     def _data_file(self, filename, encoding=None):
         filename = "data/" + filename
@@ -60,7 +53,9 @@ class TestBaseImportPdfByTemplate(common.TransactionCase):
                 "attachment_ids": attachment.ids,
             }
         )
-        return wizard
+        # We need to set is_company so that the industry_id field is visible
+        # in the res.partner form view.
+        return wizard.with_context(default_is_company=True)
 
     def _get_attachments_from_record(self, record):
         return self.env["ir.attachment"].search(
@@ -133,7 +128,6 @@ class TestBaseImportPdfByTemplate(common.TransactionCase):
         child_3 = record.child_ids.filtered(lambda x: x.name == "Child 3")
         self.assertEqual(child_3.street, "Address 3")
         self.assertEqual(child_3.country_id.code, "ES")
-        self.assertFalse(record.message_ids)
 
     def test_wizard_base_import_pdf_by_template_error(self):
         self.env.ref(
