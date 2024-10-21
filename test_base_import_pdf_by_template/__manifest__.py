ba65db7b421ca4d99fa5e237a8a3e6129f0b2b7d
Víctor Martínez <victor.martinez@tecnativa.com>
2024-10-21 09:51:48 +0200

    [IMP] base_import_pdf_by_template: Add fixed value compatibility to different types of fields
    TT51379

--- test_base_import_pdf_by_template/__manifest__.py
+++ test_base_import_pdf_by_template/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Test Base Import Pdf by Template",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "website": "https://github.com/OCA/edi",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
