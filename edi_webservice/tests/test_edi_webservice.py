8c610780857894189cf3b43580325a7047c0e269
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2024-11-18 15:58:34 +0100

    [MIG] edi_webservice: migration to V12

--- edi_webservice/tests/test_edi_webservice.py
+++ edi_webservice/tests/test_edi_webservice.py
@@ -2,7 +2,7 @@
 # @author: Enric Tobella
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.addons.edi.tests.common import EDIBackendCommonTestCase
+from odoo.addons.edi_oca.tests.common import EDIBackendCommonTestCase
 
 
 class TestEdiWebService(EDIBackendCommonTestCase):
@@ -16,6 +16,7 @@ class TestEdiWebService(EDIBackendCommonTestCase):
                 "url": "http://localhost.demo.odoo/",
                 "content_type": "application/xml",
                 "tech_name": "demo_ws",
+                "auth_type": "none",
             }
         )
         vals = {
