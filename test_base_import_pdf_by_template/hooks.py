db2914bbb12ca1457e7ccc5e730cd88898b8c34c
Víctor Martínez <victor.martinez@tecnativa.com>
2024-12-03 13:02:15 +0100

    [IMP] test_base_import_pdf_by_template: Set auto_detect_pattern

--- test_base_import_pdf_by_template/hooks.py
+++ test_base_import_pdf_by_template/hooks.py
@@ -5,17 +5,9 @@
 def post_init_hook(env):
     # Template changes
     generic_product = env.ref("test_base_import_pdf_by_template.generic_product")
-    template_po_decathlon = env.ref("test_base_import_pdf_by_template.po_decathlon")
-    template_po_decathlon.write({"auto_detect_pattern": r"(?<=ESA79935607)[\S\s]*"})
     product_model_name = "product.product"
     env.ref("test_base_import_pdf_by_template.po_decathlon_line_product_id").write(
         {"default_value": f"{product_model_name},{generic_product.id}"}
-    )
-    template_invoice_tecnativa = env.ref(
-        "test_base_import_pdf_by_template.invoice_tecnativa"
-    )
-    template_invoice_tecnativa.write(
-        {"auto_detect_pattern": r"(?<=B 8 7 5 3 0 4 3 2)[\S\s]*"}
     )
     env.ref("test_base_import_pdf_by_template.invoice_tecnativa_line_product_id").write(
         {"default_value": f"{product_model_name},{generic_product.id}"}
