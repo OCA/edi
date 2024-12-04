e7fb1acf453acec8dae19fba84006be629b96ce0
Simone Orsi <simahawk@gmail.com>
2024-12-04 07:05:35 +0100

    account_invoice_*facturx: fix factur-x lib version
    Pin 3.1 to fix https://github.com/OCA/edi/issues/1084

--- account_invoice_facturx/__manifest__.py
+++ account_invoice_facturx/__manifest__.py
@@ -17,7 +17,7 @@
         "base_facturx",
         "base_vat",
     ],
-    "external_dependencies": {"python": ["factur-x"]},
+    "external_dependencies": {"python": ["factur-x<=3.1"]},
     "data": [
         "views/res_partner.xml",
         "views/res_config_settings.xml",
