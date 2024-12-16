coopiteasy/addons

[ADD] ac_inv_ubl_mass_download: new module

account_invoice_ubl_mass_download/__manifest__.py
@@ -1,0 +1,13 @@
+# Copyright 2021 Coop IT Easy SRCLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice UBL Mass Download",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SRCLfs",
+    "website": "https://coopiteasy.be",
+    "depends": ["account_invoice_ubl"],
+    "data": ["data/ir_actions_server.xml", "data/ir_cron.xml"],
+    "demo": [],
+}

OCA/edi

account_invoice_ubl_mass_download/__manifest__.py
@@ -1,0 +1,13 @@
+# Copyright 2021 Coop IT Easy SRCLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice UBL Mass Download",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://coopiteasy.be",
+    "depends": ["account_invoice_ubl"],
+    "data": ["data/ir_actions_server.xml", "data/ir_cron.xml"],
+    "demo": [],
+}