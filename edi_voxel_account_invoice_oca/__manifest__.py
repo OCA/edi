6b75a46e447d0f4d1baa34de60a32c0bdda7ce4e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-01-29 13:57:35 +0100

    [IMP] edi_voxel_account_invoice_oca: pre-commit stuff

--- edi_voxel_account_invoice_oca/__manifest__.py
+++ edi_voxel_account_invoice_oca/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
+# Copyright 2024 Guavana - Leonardo J. Caballero G.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Voxel account invoice oca",
     "summary": "Sends account invoices to Voxel.",
-    "version": "15.0.1.0.2",
+    "version": "16.0.1.0.2",
     "development_status": "Production/Stable",
     "category": "Accounting & Finance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
