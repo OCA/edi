058a9bbd4e2f9efc573a6d0c3d67fd9cd6c5becd
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-01-30 01:11:30 +0100

    [FIX] edi_voxel_account_invoice_oca: Updated the author and year for the Copyright sentence.

--- edi_voxel_account_invoice_oca/models/res_config_settings.py
+++ edi_voxel_account_invoice_oca/models/res_config_settings.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
-# Copyright 2025, 2024 Guavana - Leonardo J. Caballero G.
+# Copyright 2024, 2025 Guavana - Leonardo J. Caballero G.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
