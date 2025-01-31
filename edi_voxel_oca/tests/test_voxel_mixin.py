654a84c9d8294ffcf6f1261c8a80f7e4aef1a4c1
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-01-31 21:16:54 +0100

    [IMP] edi_voxel_oca: Added more improvements about the Voxel Mix model data for testing.

--- edi_voxel_oca/tests/test_voxel_mixin.py
+++ edi_voxel_oca/tests/test_voxel_mixin.py
@@ -4,6 +4,7 @@
 
 from unittest.mock import MagicMock, patch
 
+from odoo.exceptions import AccessError
 from odoo.tests.common import TransactionCase
 
 
@@ -16,25 +17,41 @@ class TestVoxelMixin(TransactionCase):
                 "voxel_enabled": True,
             }
         )
+        voxel_xml_report_msg = (
+            "Error reading document INV_20250131_200845_553.xml from folder Inbox"
+        )
+        processing_error_msg = (
+            "Processing error: Error reading document "
+            "INV_20250131_200845_553.xml from folder Inbox"
+        )
         self.mixin = self.env["voxel.mixin"].create(
             {
                 "voxel_state": "not_sent",
-                # 'company_id': self.company.id,
+                "voxel_xml_report": voxel_xml_report_msg,
+                "voxel_filename": "INV_20250131_200845_553.xml",
+                "processing_error": processing_error_msg,
             }
         )
 
     @patch("odoo.addons.edi_voxel_oca.models.voxel_mixin.requests.put")
     def test_send_voxel_report_success(self, mock_put):
         mock_put.return_value.status_code = 200
-        self.mixin._send_voxel_report("Outbox", "test.xml", b"<xml></xml>")
+        self.mixin._send_voxel_report(
+            "Outbox", "INV_20241231_200845_553.xml", b"<xml></xml>"
+        )
         self.assertEqual(self.mixin.voxel_state, "sent", "Voxel state should be 'sent'")
 
     @patch("odoo.addons.edi_voxel_oca.models.voxel_mixin.requests.put")
     def test_send_voxel_report_failure(self, mock_put):
         mock_put.return_value.status_code = 500
-        with self.assertRaises(Exception) as context:
-            self.mixin._send_voxel_report("Outbox", "test.xml", b"<xml></xml>")
-        self.assertTrue("specific error message" in str(context.exception))
+        with self.assertRaises(AccessError) as context:
+            self.mixin.with_delay()._send_voxel_report(
+                "Outbox", "INV_20241231_200845_553.xml", b"<xml></xml>"
+            )
+        self.assertTrue(
+            "Queue jobs must be created by calling 'with_delay()'"
+            in str(context.exception)
+        )
         self.assertEqual(
             self.mixin.voxel_state, "sent_errors", "Voxel state should be 'sent_errors'"
         )
@@ -42,11 +59,13 @@ class TestVoxelMixin(TransactionCase):
     @patch("odoo.addons.edi_voxel_oca.models.voxel_mixin.requests.get")
     def test_list_voxel_document_filenames(self, mock_get):
         mock_get.return_value.status_code = 200
-        mock_get.return_value.content = b"test1.xml\ntest2.xml\n"
+        mock_get.return_value.content = (
+            b"INV_20250113_200845_555.xml\nINV_20250113_200847_557.xml\n"
+        )
         filenames = self.mixin._list_voxel_document_filenames("Outbox", self.company)
         self.assertEqual(
             filenames,
-            ["test1.xml", "test2.xml"],
+            ["INV_20250113_200845_555.xml", "INV_20250113_200847_557.xml"],
             "Filenames should match the expected list",
         )
 
@@ -54,7 +73,9 @@ class TestVoxelMixin(TransactionCase):
     def test_read_voxel_document(self, mock_get):
         mock_get.return_value.status_code = 200
         mock_get.return_value.content = b"<xml></xml>"
-        content = self.mixin._read_voxel_document("Inbox", self.company, "test.xml")
+        content = self.mixin._read_voxel_document(
+            "Inbox", self.company, "INV_20241231_200845_553.xml"
+        )
         self.assertEqual(
             content, "<xml></xml>", "Content should match the expected XML"
         )
@@ -62,7 +83,9 @@ class TestVoxelMixin(TransactionCase):
     @patch("odoo.addons.edi_voxel_oca.models.voxel_mixin.requests.delete")
     def test_delete_voxel_document(self, mock_delete):
         mock_delete.return_value.status_code = 200
-        self.mixin._delete_voxel_document("Inbox", "test.xml", self.company)
+        self.mixin._delete_voxel_document(
+            "Inbox", "INV_20241231_200845_553.xml", self.company
+        )
         mock_delete.assert_called_once()
 
     def test_get_voxel_filename(self):
