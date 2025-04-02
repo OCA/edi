2942aa72b4b97c28a6e81364d1c1b019cf3b9a1c
Alexis de Lattre <alexis.delattre@akretion.com>
2025-04-03 00:52:02 +0200

    [IMP] account_invoice_factux: stop pinning factur-x lib

--- account_invoice_facturx/__manifest__.py
+++ account_invoice_facturx/__manifest__.py
@@ -17,7 +17,7 @@
         "base_facturx",
         "base_vat",
     ],
-    "external_dependencies": {"python": ["factur-x<=3.1"]},
+    "external_dependencies": {"python": ["factur-x"]},
     "data": [
         "views/res_partner.xml",
         "views/res_config_settings.xml",
