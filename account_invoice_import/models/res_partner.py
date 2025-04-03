591fd9de099e236f55b52f33f8d4994b3de97f3c
Alexis de Lattre <alexis.delattre@akretion.com>
2025-04-04 00:46:41 +0200

    [IMP] account_invoice_import: re-write module according to changes proposed in bug #803
    - support multi-file in invoice import wizard
    - move fields of account.invoice.import.config to res.partner
    - remove field invoice_method (replace with invoice_import_single_line)
    - remove the "Update" scenario

--- account_invoice_import/models/res_partner.py
+++ account_invoice_import/models/res_partner.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2015-2025 Akretion France (https://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -8,37 +8,68 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    invoice_import_ids = fields.One2many(
-        "account.invoice.import.config",
-        "partner_id",
-        string="Invoice Import Configuration",
+    # DEFAULT VALUE fields
+    invoice_import_product_id = fields.Many2one(
+        "product.product", string="Default Product", company_dependent=True
     )
-    invoice_import_count = fields.Integer(
-        compute="_compute_invoice_import_count",
-        string="Number of Invoice Import Configurations",
+    # only if invoice_import_product_id is not set
+    invoice_import_account_id = fields.Many2one(
+        "account.account",
+        company_dependent=True,
+        string="Default Expense Account",
+        domain="[('deprecated', '=', False), ('company_id', '=', current_company_id)]",
+        help="The account configured here will go through the mapping of the fiscal position.",
+    )
+    # only if invoice_import_product_id is not set
+    invoice_import_tax_ids = fields.Many2many(
+        "account.tax",
+        string="Default Taxes",
+        domain="[('type_tax_use', '=', 'purchase'), ('company_id', '=', current_company_id)]",
+        help="Taxes configured here will go through the mapping of the fiscal position.",
+    )
+    # FORCE VALUE fields
+    # company_dependent=True ??
+    invoice_import_single_line = fields.Boolean(string="Force Single Invoice Line")
+    invoice_import_label = fields.Char(
+        string="Force Invoice Line Description", help="Force Invoice Line Description"
     )
 
-    def _compute_invoice_import_count(self):
-        config_data = self.env["account.invoice.import.config"].read_group(
-            [("partner_id", "in", self.ids), ("company_id", "=", self.env.company.id)],
-            ["partner_id"],
-            ["partner_id"],
-        )
-        mapped_data = {
-            config["partner_id"][0]: config["partner_id_count"]
-            for config in config_data
-        }
-        for partner in self:
-            partner.invoice_import_count = mapped_data.get(partner.id, 0)
+    invoice_import_journal_id = fields.Many2one(
+        "account.journal",
+        string="Force Purchase Journal",
+        company_dependent=True,
+        domain="[('type', '=', 'purchase'), ('company_id', '=', current_company_id)]",
+        help="If empty, Odoo will use the first purchase journal.",
+    )
+    # For analytic, users should use the ability to auto-set an analytic distribution
+    # depending on product/partner
 
-    def show_account_invoice_import_config(self):
+    def _convert_to_import_config(self, company):
         self.ensure_one()
-        xmlid = "account_invoice_import.account_invoice_import_config_action"
-        action = self.env["ir.actions.actions"]._for_xml_id(xmlid)
-        action["context"] = {
-            "default_name": self.name,
-            "default_partner_id": self.id,
-            "search_default_partner_id": self.id,
-            "invoice_import_config_main_view": True,
+        if not company:
+            company = self.env.company
+        self = self.with_company(company.id)
+        vals = {
+            "company": company,
+            "single_line": self.invoice_import_single_line,
+            "label": self.invoice_import_label or False,
+            "journal": self.invoice_import_journal_id or False,
         }
-        return action
+        if self.invoice_import_product_id:
+            vals["product"] = self.invoice_import_product_id
+        else:
+            taxes = (
+                self.invoice_import_tax_ids
+                and self.invoice_import_tax_ids.filtered(
+                    lambda tax: tax.company_id == company
+                )
+                or False
+            )
+            if taxes:
+                vals["taxes"] = taxes
+            if (
+                self.invoice_import_account_id
+                and self.invoice_import_account_id.company_id == company
+            ):
+                vals["account"] = self.invoice_import_account_id
+        return vals
