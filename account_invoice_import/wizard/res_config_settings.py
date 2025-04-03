3dda41720489df9a3dc6da6d4ff31bfd40fc0985
Alexis de Lattre <alexis.delattre@akretion.com>
2025-04-04 00:46:41 +0200

    [IMP] account_invoice_import: re-write module according to changes proposed in bug #803
    - support multi-file in invoice import wizard
    - move fields of account.invoice.import.config to res.partner
    - remove field invoice_method (replace with invoice_import_single_line)
    - remove the "Update" scenario

--- account_invoice_import/wizard/res_config_settings.py
+++ account_invoice_import/wizard/res_config_settings.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2017-2021 Akretion France (https://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -9,10 +9,16 @@ class AccountConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     adjustment_credit_account_id = fields.Many2one(
-        related="company_id.adjustment_credit_account_id", readonly=False
+        related="company_id.adjustment_credit_account_id",
+        readonly=False,
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id), "
+        "('account_type', '=like', 'income%')]",
     )
     adjustment_debit_account_id = fields.Many2one(
-        related="company_id.adjustment_debit_account_id", readonly=False
+        related="company_id.adjustment_debit_account_id",
+        readonly=False,
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id), "
+        "('account_type', '=like', 'expense%')]",
     )
     invoice_import_email = fields.Char(
         related="company_id.invoice_import_email", readonly=False
