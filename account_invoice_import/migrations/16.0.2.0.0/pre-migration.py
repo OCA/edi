cf7f63935a073df601b19f66123757eb1c637c82
Alexis de Lattre <alexis.delattre@akretion.com>
2025-04-14 00:19:47 +0200

    [IMP] account_invoice_import: add migration script

---
+++ account_invoice_import/migrations/16.0.2.0.0/pre-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Akretion France (https://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_tables(
+        env.cr,
+        [
+            ("account_invoice_import_config", None),
+        ],
+    )
