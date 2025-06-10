2728abfeb97de3fceeb4e484edd60055ce24a999
Simone Orsi <simahawk@gmail.com>
2025-06-10 08:09:44 +0200

    base_ubl: split in base_ubl_parse + base_ubl_generate

--- base_ubl/__manifest__.py
+++ base_ubl/__manifest__.py
@@ -7,8 +7,8 @@
     "category": "Hidden",
     "license": "AGPL-3",
     "summary": "Base module for Universal Business Language (UBL)",
-    "author": "Akretion,Onestein,Odoo Community Association (OCA)",
+    "author": "Akretion,Onestein,Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
-    "depends": ["uom_unece", "account_tax_unece", "base_vat", "pdf_helper"],
+    "depends": ["base"],
     "installable": True,
 }
