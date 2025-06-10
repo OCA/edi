ed3c5e18d9e8810c355835e9f3ad868ba704fc91
Simone Orsi <simahawk@gmail.com>
2025-06-10 13:00:17 +0200

    base_ubl_generate: migrate to v18

--- base_ubl_generate/__manifest__.py
+++ base_ubl_generate/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Base UBL generate",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
     "summary": "Base module to generate UBL files (Universal Business Language)",
