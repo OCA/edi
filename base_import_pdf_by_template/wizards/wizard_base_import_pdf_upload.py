9459ffc0751c6ade2933db5077a719ba2de1184d
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-01 10:39:40 +0200

    [IMP] base_import_pdf_by_template: Add logger.info to exception wizard process
    It is very important to have a log of the exception to better understand what is
    happening (a badly formatted value or a field that does not exist in the view
    by the user who is executing it).
    
    TT56274

--- base_import_pdf_by_template/wizards/wizard_base_import_pdf_upload.py
+++ base_import_pdf_by_template/wizards/wizard_base_import_pdf_upload.py
@@ -75,7 +75,8 @@ class WizardBaseImportPdfUpload(models.TransientModel):
         for line in self.line_ids.filtered("template_id"):
             try:
                 records += line.action_process()
-            except Exception:
+            except Exception as e:
+                logger.info(e)
                 if not self.env.context.get("skip_template_not_found_error"):
                     raise
         action = {
@@ -190,7 +191,8 @@ class WizardBaseImportPdfUploadLine(models.TransientModel):
         else:
             try:
                 setattr(_form, field_name, value)
-            except Exception:
+            except Exception as e:
+                logger.info(e)
                 self._add_log_error_text(field_name, value)
 
     def _process_form(self):
@@ -217,7 +219,8 @@ class WizardBaseImportPdfUploadLine(models.TransientModel):
                     field_value = model[field_name].browse(field_value)
                 try:
                     setattr(model_form, field_name, field_value)
-                except Exception:
+                except Exception as e:
+                    logger.info(e)
                     extra_vals[field_name] = ctx[key]
         # Set the values of the header in Form
         header_values = template._get_field_header_values(text)
@@ -240,7 +243,8 @@ class WizardBaseImportPdfUploadLine(models.TransientModel):
                     child_field_value = child_fixed_values[field_name]
                     try:
                         setattr(line_form, field_name, child_field_value)
-                    except Exception:
+                    except Exception as e:
+                        logger.info(e)
                         self._add_log_error_text(field_name, child_field_value)
                 # set the values of any line
                 for field_name in list(line.keys()):
