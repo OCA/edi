406eed20af816d33d6e4cc2204282fc3640d9c6f
SilvioC2C <silvio.gregorini@camptocamp.com>
2026-06-11 16:38:41 +0200

    [IMP] purchase_order_import: pre-commit auto fixes

--- purchase_order_import/models/purchase_order.py
+++ purchase_order_import/models/purchase_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,6 @@ from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
-
     _inherit = "purchase.order"
 
     supplier_ack_dt = fields.Datetime(
