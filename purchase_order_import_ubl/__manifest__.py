2021d3021a78c7c5ed682f1950c1de49588ea4e8
SilvioC2C <silvio.gregorini@camptocamp.com>
2026-06-11 21:15:21 +0200

    [IMP] purchase_order_import_ubl: pre-commit auto fixes

--- purchase_order_import_ubl/__manifest__.py
+++ purchase_order_import_ubl/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Quotation Order UBL Import',
-    'version': '10.0.2.0.2',
-    'category': 'Purchase Management',
-    'license': 'AGPL-3',
-    'summary': 'Import UBL XML quotation files',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
-    'depends': ['purchase_order_import', 'base_ubl'],
-    'demo': ['demo/demo_data.xml'],
-    'installable': True,
+    "name": "Quotation Order UBL Import",
+    "version": "10.0.2.0.2",
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "summary": "Import UBL XML quotation files",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["purchase_order_import", "base_ubl"],
+    "demo": ["demo/demo_data.xml"],
+    "installable": True,
 }
