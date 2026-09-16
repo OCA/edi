bb124f29b025406b6cf455cd961435ad6a969f1e
Alexis de Lattre <alexis.delattre@akretion.com>
2026-09-17 00:36:25 +0200

    [IMP] account_invoice_import: add a method to create an invoice from json+rpc

--- account_invoice_import/wizard/account_invoice_import.py
+++ account_invoice_import/wizard/account_invoice_import.py
@@ -898,6 +898,12 @@ class AccountInvoiceImport(models.TransientModel):
         return invoice
 
     @api.model
+    def create_invoice_rpc(self, parsed_inv, company_id, origin=None):
+        company = self.env["res.company"].browse(company_id)
+        import_config = {"company": company}
+        return self.create_invoice(parsed_inv, import_config, origin=origin)
+
+    @api.model
     def create_invoice_webservice(
         self,
         invoice_file_b64,
